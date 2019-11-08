4c918b44771424df969af8c54c32e2c3263ab763
Akshay Narayan <akshayn@mit.edu>
2019-11-08 15:40:27 +0000

    make clippy shut up about complex types

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
-//! Container manager
+//! Add shared filesystem for pipes to new containers,
+//! and translate between service-level addresses and
+//! pipes.
+
 #![warn(clippy::all)]
+#![allow(clippy::type_complexity)]
 
 pub fn logger() -> slog::Logger {
     use slog::Drain;
