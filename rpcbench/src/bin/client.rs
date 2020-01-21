87f119a0a0bbb30cf34c464e734c47384b14c8a0
Akshay Narayan <akshayn@mit.edu>
2020-01-21 22:22:58 +0000

    rpcbench: Support raw TCP in client

--- rpcbench/src/bin/client.rs
+++ rpcbench/src/bin/client.rs
@@ -21,39 +21,30 @@ async fn main() -> Result<(), failure::Error> {
     let log = burrito_ctl::logger();
     let opt = Opt::from_args();
 
+    let pp = rpcbench::PingParams {
+        work: rpcbench::Work::from_i32(opt.work)
+            .ok_or_else(|| failure::format_err!("Invalid work value"))? as i32,
+        amount: opt.amount,
+    };
+
     let durs = if let Some(root) = opt.burrito_root {
         // burrito mode
         let cl = burrito_addr::Client::new(root, &log).await?;
         let addr: hyper::Uri = burrito_addr::Uri::new(&opt.addr).into();
         trace!(&log, "Connecting to rpcserver"; "addr" => ?&addr);
-        rpcbench::client_ping(
-            addr,
-            cl,
-            rpcbench::PingParams {
-                work: rpcbench::Work::from_i32(opt.work)
-                    .ok_or_else(|| failure::format_err!("Invalid work value"))?
-                    as i32,
-                amount: opt.amount,
-            },
-            opt.iters,
-        )
-        .await?
+        rpcbench::client_ping(addr, cl, pp, opt.iters).await?
+    } else if opt.addr.starts_with("http") {
+        // raw tcp mode
+        use std::str::FromStr;
+        let http = hyper::client::connect::HttpConnector::new();
+        let addr: hyper::Uri = hyper::Uri::from_str(&opt.addr)?;
+
+        rpcbench::client_ping(addr, http, pp, opt.iters).await?
     } else {
         // raw unix mode
         let addr: hyper::Uri = hyper_unix_connector::Uri::new(opt.addr, "/").into();
         trace!(&log, "Connecting to rpcserver"; "addr" => ?&addr);
-        rpcbench::client_ping(
-            addr,
-            hyper_unix_connector::UnixClient,
-            rpcbench::PingParams {
-                work: rpcbench::Work::from_i32(opt.work)
-                    .ok_or_else(|| failure::format_err!("Invalid work value"))?
-                    as i32,
-                amount: opt.amount,
-            },
-            opt.iters,
-        )
-        .await?
+        rpcbench::client_ping(addr, hyper_unix_connector::UnixClient, pp, opt.iters).await?
     };
 
     println!("Total_us,Server_us");
