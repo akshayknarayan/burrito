adacabeecd4be4d31311fde859c829e5e78d4660
Akshay Narayan <akshayn@mit.edu>
2020-01-29 17:15:44 +0000

    build.rs: Add proto files to rerun-if-changed

--- burrito-addr/build.rs
+++ burrito-addr/build.rs
@@ -1,4 +1,5 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("rerun-if-changed=../proto/burrito.proto");
     tonic_build::compile_protos("../proto/burrito.proto")?;
     Ok(())
 }
