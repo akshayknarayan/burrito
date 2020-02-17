c50dba1a19e55d9033ac6fdac8afbd775e24ba9d
Akshay Narayan <akshayn@mit.edu>
2020-02-17 17:40:11 +0000

    util: Use byteorder::read_u32 instead of mem::transmute

--- burrito-util/src/lib.rs
+++ burrito-util/src/lib.rs
@@ -2,13 +2,15 @@
 //!
 //! Will wrap the buffer with its length.
 
+use byteorder::{ByteOrder, LittleEndian};
+
 pub async fn write_msg(
     mut cl: impl tokio::io::AsyncWrite + Unpin,
     msg_type: Option<u32>,
     msg: &[u8],
 ) -> Result<(), failure::Error> {
     use tokio::io::AsyncWriteExt;
-    cl.write_all(&msg.len().to_le_bytes()).await?;
+    cl.write_all(&(msg.len() as u32).to_le_bytes()).await?;
     if let Some(t) = msg_type {
         cl.write_all(&t.to_le_bytes()).await?;
     }
@@ -23,11 +25,7 @@ async fn read_u32(
 ) -> Result<u32, failure::Error> {
     use tokio::io::AsyncReadExt;
     cl.read_exact(&mut buf[0..4]).await?;
-
-    // unsafe transmute ok because endianness is guaranteed to be the
-    // same on either side of a UDS
-    // will read only the first sizeof(u32) = 4 bytes
-    Ok(unsafe { std::mem::transmute_copy(&buf) })
+    Ok(LittleEndian::read_u32(buf))
 }
 
 async fn read_len(
@@ -35,12 +33,16 @@ async fn read_len(
     buf: &mut [u8],
 ) -> Result<usize, failure::Error> {
     Ok(loop {
-        let len_to_read = read_u32(&mut cl, &mut *buf).await? as usize;
+        let len_to_read = read_u32(&mut cl, buf).await? as usize;
         if len_to_read == 0 {
             tokio::task::yield_now().await;
             continue;
         } else if len_to_read > buf.len() {
-            failure::bail!("message size too large: {:?} > 128", len_to_read);
+            failure::bail!(
+                "message size too large: {:?} > {:?}",
+                len_to_read,
+                buf.len()
+            );
         } else {
             break len_to_read;
         }
@@ -53,7 +55,7 @@ pub async fn read_msg(
 ) -> Result<usize, failure::Error> {
     use tokio::io::AsyncReadExt;
 
-    let len_to_read = read_len(&mut cl, &mut *buf).await?;
+    let len_to_read = read_len(&mut cl, buf).await?;
     cl.read_exact(&mut buf[0..len_to_read]).await?;
     Ok(len_to_read)
 }
@@ -64,8 +66,8 @@ pub async fn read_msg_with_type(
 ) -> Result<(usize, u32), failure::Error> {
     use tokio::io::AsyncReadExt;
 
-    let len_to_read = read_len(&mut cl, &mut *buf).await?;
-    let msg_type = read_u32(&mut cl, &mut *buf).await?;
+    let len_to_read = read_len(&mut cl, buf).await?;
+    let msg_type = read_u32(&mut cl, buf).await?;
     cl.read_exact(&mut buf[0..len_to_read as usize]).await?;
     Ok((len_to_read as usize, msg_type))
 }
@@ -94,5 +96,56 @@ mod test {
         assert_eq!(BAR, 1);
         assert_eq!(BAZ, 2);
         assert_eq!(QUX, 3);
+    }
+
+    #[test]
+    fn read_write() -> Result<(), Box<dyn std::error::Error>> {
+        let mut rt = tokio::runtime::Builder::new()
+            .basic_scheduler()
+            .enable_io()
+            .build()?;
+
+        let r: Result<(), Box<dyn std::error::Error>> = rt.block_on(async move {
+            let mut buf = vec![];
+            super::write_msg(&mut buf, Some(1), b"foo").await?;
+
+            println!("wrote: {:?}", buf);
+
+            let mut read_buf = [0u8; 16];
+            let cur = std::io::Cursor::new(buf);
+            let (len, typ) = super::read_msg_with_type(cur, &mut read_buf).await?;
+
+            assert_eq!(typ, 1);
+            assert_eq!(&read_buf[0..len as usize], b"foo");
+            Ok(())
+        });
+        r?;
+
+        Ok(())
+    }
+
+    #[test]
+    fn read_write_notype() -> Result<(), Box<dyn std::error::Error>> {
+        let mut rt = tokio::runtime::Builder::new()
+            .basic_scheduler()
+            .enable_io()
+            .build()?;
+
+        let r: Result<(), Box<dyn std::error::Error>> = rt.block_on(async move {
+            let mut buf = vec![];
+            super::write_msg(&mut buf, None, b"foo").await?;
+
+            println!("wrote: {:?}", buf);
+
+            let mut read_buf = [0u8; 16];
+            let cur = std::io::Cursor::new(buf);
+            let len = super::read_msg(cur, &mut read_buf).await?;
+
+            assert_eq!(&read_buf[0..len as usize], b"foo");
+            Ok(())
+        });
+        r?;
+
+        Ok(())
     }
 }
