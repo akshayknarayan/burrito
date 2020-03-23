e14a3351c9e7df8e12ac3c14b7bdc47a565a6c71
Akshay Narayan <akshayn@mit.edu>
2020-03-23 23:45:45 +0000

    burrito-shard-ctl: Server

---
+++ burrito-shard-ctl/src/lib.rs
@@ -0,0 +1,188 @@
+use anyhow::Error;
+use std::collections::HashMap;
+use std::pin::Pin;
+use std::sync::Arc;
+use tokio::sync::{Mutex, RwLock};
+use tracing::{trace, warn};
+
+pub mod proto;
+
+/// Service to keep track of sharded services.
+///
+/// Install XDP programs to do the sharding as needed.
+#[derive(Clone)]
+pub struct ShardCtl {
+    shard_table: Arc<RwLock<HashMap<String, proto::ShardInfo>>>,
+    redis_client: redis::Client,
+    redis_listen_connection: Arc<Mutex<redis::aio::Connection>>,
+}
+
+impl ShardCtl {
+    pub async fn new(redis_addr: &str) -> Result<Self, Error> {
+        let redis_client = redis::Client::open(redis_addr)?;
+        let redis_listen_connection =
+            Arc::new(Mutex::new(redis_client.get_async_connection().await?));
+
+        let s = ShardCtl {
+            redis_client,
+            redis_listen_connection,
+            shard_table: Default::default(),
+        };
+
+        Ok(s)
+    }
+
+    pub async fn serve_on<S, E>(self, sk: S) -> Result<(), Error>
+    where
+        S: tokio::stream::Stream<Item = Result<tokio::net::UnixStream, E>>,
+        E: std::error::Error,
+    {
+        use futures_util::{sink::Sink, stream::StreamExt};
+        let con = self
+            .redis_client
+            .get_async_connection()
+            .await
+            .expect("get redis connection");
+        tokio::spawn(listen_updates(self.shard_table.clone(), con));
+
+        sk.for_each_concurrent(None, |st| async {
+            let st: async_bincode::AsyncBincodeStream<_, proto::Request, proto::Reply, _> =
+                st.expect("accept failed").into();
+            let mut st = st.for_async();
+            loop {
+                let req = st.next().await;
+                let rep = async {
+                    match req {
+                        Some(Ok(proto::Request::Register(si))) => {
+                            let rep = self.register(si).await;
+                            Ok(proto::Reply::Register(rep))
+                        }
+                        Some(Ok(proto::Request::Query(sa))) => {
+                            let rep = self.query(sa).await;
+                            Ok(proto::Reply::Query(rep))
+                        }
+                        Some(Err(e)) => Err(anyhow::Error::from(e)),
+                        None => Err(anyhow::anyhow!("Stream done")),
+                    }
+                };
+
+                let (rep, rdy): (Result<proto::Reply, Error>, _) = futures_util::future::join(
+                    rep,
+                    futures_util::future::poll_fn(|cx| {
+                        let pst = Pin::new(&mut st);
+                        pst.poll_ready(cx)
+                    }),
+                )
+                .await;
+                rdy.unwrap();
+
+                if let Err(_) = rep {
+                    break;
+                }
+
+                let pst = Pin::new(&mut st);
+                pst.start_send(rep.unwrap()).unwrap();
+            }
+        })
+        .await;
+        Ok(())
+    }
+
+    // Instead of tower_service::call(), do this, which lets us have async fns
+    // instead of fn foo() -> Pin<Box<dyn Future<..>>> everywhere
+    pub async fn register(&self, req: proto::ShardInfo) -> proto::RegisterShardReply {
+        let redis_conn = self.redis_listen_connection.clone();
+        if let Err(e) = self.shard_table_insert(req.clone()).await {
+            return proto::RegisterShardReply::Err(format!(
+                "Could not insert shard-service: {}",
+                e
+            ));
+        }
+
+        tokio::spawn(async move {
+            let r = req.clone();
+            if let Err(e) = redis_insert(redis_conn, &r).await {
+                warn!(req = ?&r, err = ?e, "Could not do redis insert");
+            }
+        });
+
+        proto::RegisterShardReply::Ok
+    }
+
+    pub async fn query(&self, req: proto::QueryShardRequest) -> proto::QueryShardReply {
+        if let Some(s) = self.shard_table.read().await.get(&req.service_name) {
+            proto::QueryShardReply::Ok(s.clone())
+        } else {
+            proto::QueryShardReply::Err(format!("Could not find {:?}", &req.service_name))
+        }
+    }
+
+    async fn shard_table_insert(&self, serv: proto::ShardInfo) -> Result<(), Error> {
+        let sn = serv.service_name.clone();
+        if let Some(s) = self.shard_table.write().await.insert(sn.clone(), serv) {
+            Err(anyhow::anyhow!(
+                "Shard service address {} already in use at: {:?}",
+                sn,
+                s.canonical_addr
+            ))?;
+        };
+
+        Ok(())
+    }
+}
+async fn listen_updates(
+    shard_table: Arc<RwLock<HashMap<String, proto::ShardInfo>>>,
+    mut con: redis::aio::Connection,
+) {
+    // usual blah blah warning about polling intervals
+    let mut interval = tokio::time::interval(std::time::Duration::from_millis(100));
+    loop {
+        interval.tick().await;
+        if let Err(e) = poll_updates(&shard_table, &mut con).await {
+            warn!(err = ?e, "Failed to poll shard-services updates");
+        } else {
+            trace!("Updated shard-services");
+        }
+    }
+}
+
+async fn poll_updates(
+    shard_table: &RwLock<HashMap<String, proto::ShardInfo>>,
+    con: &mut redis::aio::Connection,
+) -> Result<(), Error> {
+    use redis::AsyncCommands;
+    let srvs: Vec<String> = con.smembers("shard-services").await?;
+
+    // TODO fast-path check if a write is even necessary?
+
+    let mut tbl = shard_table.write().await;
+    for srv in srvs {
+        if !tbl.contains_key(&srv) {
+            let sh_info_blob = con.get::<_, Vec<u8>>(&srv).await?;
+            let sh_info = bincode::deserialize(&sh_info_blob)?;
+            let k = srv.trim_start_matches("shard:");
+            tbl.insert(k.to_owned(), sh_info);
+        }
+    }
+
+    Ok(())
+}
+
+async fn redis_insert(
+    conn: Arc<Mutex<redis::aio::Connection>>,
+    serv: &proto::ShardInfo,
+) -> Result<(), Error> {
+    let name = format!("shard:{}", serv.service_name);
+    let mut r = redis::pipe();
+    r.atomic()
+        .cmd("SADD")
+        .arg("shard-services")
+        .arg(&name)
+        .ignore();
+
+    let shard_blob = bincode::serialize(serv)?;
+    r.cmd("SET").arg(&name).arg(shard_blob).ignore();
+
+    r.query_async(&mut *conn.lock().await).await?;
+    Ok(())
+}
---
+++ burrito-shard-ctl/src/proto.rs
@@ -0,0 +1,56 @@
+use serde::{Deserialize, Serialize};
+use std::net::SocketAddr;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum Addr {
+    Burrito(String),
+    Tcp(SocketAddr),
+    Udp(SocketAddr),
+}
+
+#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
+pub struct SimpleShardPolicy {
+    pub packet_data_offset: u8,
+    pub packet_data_length: u8,
+}
+
+/// Request type for servers registering.
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ShardInfo {
+    pub service_name: String,
+    pub canonical_addr: Addr,
+    pub shard_addrs: Vec<Addr>,
+    pub shard_info: SimpleShardPolicy,
+}
+
+/// Response to a ShardInfo request which registers a shard.
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum RegisterShardReply {
+    Err(String),
+    Ok,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct QueryShardRequest {
+    pub service_name: String,
+}
+
+/// Response type for clients wanting to know where the servers are.
+/// The Ok variant is the same as the server request type.
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum QueryShardReply {
+    Err(String),
+    Ok(ShardInfo),
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum Request {
+    Register(ShardInfo),
+    Query(QueryShardRequest),
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum Reply {
+    Register(RegisterShardReply),
+    Query(QueryShardReply),
+}
