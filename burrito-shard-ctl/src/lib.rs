c714a387cf181049e3a6bc2d3918850e523207bd
Akshay Narayan <akshayn@mit.edu>
2020-03-24 15:18:05 +0000

    shard-ctl: Add client

--- burrito-shard-ctl/src/lib.rs
+++ burrito-shard-ctl/src/lib.rs
@@ -1,5 +1,6 @@
 use anyhow::Error;
 use std::collections::HashMap;
+use std::path::Path;
 use std::pin::Pin;
 use std::sync::Arc;
 use tokio::sync::{Mutex, RwLock};
@@ -7,9 +8,91 @@ use tracing::{trace, warn};
 
 pub mod proto;
 
-/// Service to keep track of sharded services.
-///
-/// Install XDP programs to do the sharding as needed.
+pub const CONTROLLER_ADDRESS: &str = "shard-controller";
+
+pub struct ShardCtlClient {
+    uc: async_bincode::AsyncBincodeStream<
+        tokio::net::UnixStream,
+        proto::Reply,
+        proto::Request,
+        async_bincode::AsyncDestination,
+    >,
+}
+
+impl ShardCtlClient {
+    pub async fn new(burrito_root: impl AsRef<Path>) -> Result<Self, Error> {
+        let controller_addr = burrito_root.as_ref().join(CONTROLLER_ADDRESS);
+        let uc: async_bincode::AsyncBincodeStream<_, proto::Reply, proto::Request, _> =
+            tokio::net::UnixStream::connect(controller_addr)
+                .await?
+                .into();
+        let uc = uc.for_async();
+
+        Ok(ShardCtlClient { uc })
+    }
+
+    pub async fn register(&mut self, req: proto::ShardInfo) -> Result<(), Error> {
+        use futures_util::{sink::Sink, stream::StreamExt};
+        futures_util::future::poll_fn(|cx| {
+            let pst = Pin::new(&mut self.uc);
+            pst.poll_ready(cx)
+        })
+        .await?;
+
+        let pst = Pin::new(&mut self.uc);
+        pst.start_send(proto::Request::Register(req))?;
+
+        futures_util::future::poll_fn(|cx| {
+            let pst = Pin::new(&mut self.uc);
+            pst.poll_flush(cx)
+        })
+        .await?;
+
+        // now wait for the response
+        match self.uc.next().await {
+            Some(Ok(proto::Reply::Register(proto::RegisterShardReply::Ok))) => Ok(()),
+            Some(Ok(proto::Reply::Register(proto::RegisterShardReply::Err(e)))) => {
+                Err(anyhow::anyhow!(e))
+            }
+            Some(Err(e)) => Err(anyhow::Error::from(e)),
+            None => Err(anyhow::anyhow!("Stream done")),
+            Some(Ok(proto::Reply::Query(_))) => unreachable!(),
+        }
+    }
+
+    pub async fn query(&mut self, req: &str) -> Result<proto::ShardInfo, Error> {
+        use futures_util::{sink::Sink, stream::StreamExt};
+        futures_util::future::poll_fn(|cx| {
+            let pst = Pin::new(&mut self.uc);
+            pst.poll_ready(cx)
+        })
+        .await?;
+
+        let pst = Pin::new(&mut self.uc);
+        pst.start_send(proto::Request::Query(proto::QueryShardRequest {
+            service_name: req.into(),
+        }))?;
+
+        futures_util::future::poll_fn(|cx| {
+            let pst = Pin::new(&mut self.uc);
+            pst.poll_flush(cx)
+        })
+        .await?;
+
+        // now wait for the response
+        match self.uc.next().await {
+            Some(Ok(proto::Reply::Query(proto::QueryShardReply::Ok(si)))) => Ok(si),
+            Some(Ok(proto::Reply::Query(proto::QueryShardReply::Err(e)))) => {
+                Err(anyhow::anyhow!(e))
+            }
+            Some(Err(e)) => Err(anyhow::Error::from(e)),
+            None => Err(anyhow::anyhow!("Stream done")),
+            Some(Ok(proto::Reply::Register(_))) => unreachable!(),
+        }
+    }
+}
+
+/// Keep track of sharded services.
 #[derive(Clone)]
 pub struct ShardCtl {
     shard_table: Arc<RwLock<HashMap<String, proto::ShardInfo>>>,
