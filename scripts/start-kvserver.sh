1d001210707bc4ed9843d4a98010cbd24b099796
Akshay Narayan <akshayn@mit.edu>
2020-03-27 19:16:01 +0000

    Don't filter getifaddrs() interfaces by port

--- scripts/start-kvserver.sh
+++ scripts/start-kvserver.sh
@@ -3,6 +3,6 @@
 sudo DOCKER_HOST=unix:///var/run/burrito-docker.sock docker rm -f burrito-shard-redis
 sudo DOCKER_HOST=unix:///var/run/burrito-docker.sock docker run --name burrito-shard-redis -d -p 6379:6379 redis:5
 sleep 5
-RUST_LOG=debug ./target/release/burrito-shard -f -b /tmp/burrito -r "redis://localhost:6379" &
+sudo RUST_LOG=debug ./target/release/burrito-shard -f -b /tmp/burrito -r "redis://localhost:6379" &
 sleep 5
-./target/release/kvserver -i $1 -p $2 -n $3
+sudo ./target/release/kvserver -i $1 -p $2 -n $3
