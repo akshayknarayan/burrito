34fd9ac188d7b8f4c54ea44137c353135e4e491b
Akshay Narayan <akshayn@mit.edu>
2020-04-01 00:52:43 +0000

    tmp

--- scripts/start-kvclient.sh
+++ scripts/start-kvclient.sh
@@ -7,13 +7,15 @@
 sudo pkill -9 burrito-shard
 sudo rm -rf /tmp/burrito/*
 
-RUST_LOG=info ./target/release/burrito-shard -r redis://$1:6379 &
+RUST_LOG=info ./target/release/burrito-shard -r "redis://$1:6379" &
+
+sleep 5
 
 sudo RUST_LOG=info cset shield --userset=kv --exec ./target/release/ycsb -- \
             --burrito-root="/tmp/burrito" \
             --addr "kv" \
             --accesses ./kvstore-ycsb/ycsbc-mock/wrkloadb1-100.access \
-            -i $2 -n $3 &
+            -i "$2" -n "$3" &
 client1=$!
 echo "started client 1"
 
@@ -21,10 +23,10 @@ RUST_LOG=info ./target/release/ycsb \
     --burrito-root="/tmp/burrito" \
     --addr "kv" \
     --accesses ./kvstore-ycsb/ycsbc-mock/wrkloadb2-100.access \
-    -i $2 -n $3 &
+    -i "$2" -n "$3" &
 client2=$!
 echo "started client 2"
 
-wait $client1 $client2
+wait "$client1" "$client2"
 sudo pkill -9 burrito-shard
 sudo rm -rf /tmp/burrito/*
