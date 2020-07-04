ad0145fae0671b9b86af91a49861da4d61c84b3e
Akshay Narayan <akshayn@mit.edu>
2020-07-04 19:19:22 +0000

    bertha: Retransmissions

--- bertha/src/chan_transport.rs
+++ bertha/src/chan_transport.rs
@@ -6,16 +6,18 @@ use std::future::Future;
 use std::pin::Pin;
 use std::sync::Arc;
 use tokio::sync::{mpsc, Mutex};
+use tracing::debug;
 
 pub struct Chan<Data> {
     snd1: Option<mpsc::Sender<Data>>,
     rcv1: Option<mpsc::Receiver<Data>>,
     snd2: Option<mpsc::Sender<Data>>,
     rcv2: Option<mpsc::Receiver<Data>>,
+    loss: Arc<dyn Fn() -> bool + Send + Sync + 'static>,
 }
 
-impl Chan<Vec<u8>> {
-    pub fn new() -> Self {
+impl Default for Chan<Vec<u8>> {
+    fn default() -> Self {
         let (s1, r1) = mpsc::channel(100);
         let (s2, r2) = mpsc::channel(100);
         Self {
@@ -23,7 +25,15 @@ impl Chan<Vec<u8>> {
             rcv1: Some(r1),
             snd2: Some(s2),
             rcv2: Some(r2),
+            loss: Arc::new(|| false),
         }
+    }
+}
+
+impl<T> Chan<T> {
+    pub fn lossy(&mut self, loss: impl Fn() -> bool + Send + Sync + 'static) -> &mut Self {
+        self.loss = Arc::new(loss);
+        self
     }
 }
 
@@ -38,12 +48,20 @@ where
         &mut self,
         _: Self::Addr,
     ) -> Pin<Box<dyn Future<Output = Pin<Box<dyn Stream<Item = Self::Connection>>>>>> {
-        let r = ChanChunnel::new(self.snd1.take().unwrap(), self.rcv2.take().unwrap());
+        let r = ChanChunnel::new(
+            self.snd1.take().unwrap(),
+            self.rcv2.take().unwrap(),
+            Arc::clone(&self.loss),
+        );
         Box::pin(async move { Box::pin(futures_util::stream::once(async { r })) as _ })
     }
 
     fn connect(&mut self, _: Self::Addr) -> Pin<Box<dyn Future<Output = Self::Connection>>> {
-        let r = ChanChunnel::new(self.snd2.take().unwrap(), self.rcv1.take().unwrap());
+        let r = ChanChunnel::new(
+            self.snd2.take().unwrap(),
+            self.rcv1.take().unwrap(),
+            Arc::clone(&self.loss),
+        );
         Box::pin(async { r })
     }
 }
@@ -51,13 +69,19 @@ where
 pub struct ChanChunnel<Data> {
     snd: Arc<Mutex<mpsc::Sender<Data>>>,
     rcv: Arc<Mutex<mpsc::Receiver<Data>>>,
+    loss: Arc<dyn Fn() -> bool + Send + Sync + 'static>,
 }
 
 impl<D> ChanChunnel<D> {
-    fn new(s: mpsc::Sender<D>, r: mpsc::Receiver<D>) -> Self {
+    fn new(
+        s: mpsc::Sender<D>,
+        r: mpsc::Receiver<D>,
+        loss: Arc<dyn Fn() -> bool + Send + Sync + 'static>,
+    ) -> Self {
         Self {
             snd: Arc::new(Mutex::new(s)),
             rcv: Arc::new(Mutex::new(r)),
+            loss,
         }
     }
 }
@@ -73,8 +97,14 @@ where
         data: Self::Data,
     ) -> Pin<Box<dyn Future<Output = Result<(), eyre::Report>> + Send + Sync>> {
         let s = self.snd.clone();
+        let drop = (self.loss)();
+
         Box::pin(async move {
-            s.lock().await.send(data).await.map_err(|_| ()).unwrap();
+            if !drop {
+                s.lock().await.send(data).await.map_err(|_| ()).unwrap();
+            } else {
+                debug!("dropping message");
+            }
             Ok(())
         })
     }
--- bertha/src/reliable.rs
+++ bertha/src/reliable.rs
@@ -18,27 +18,45 @@ use tracing_futures::Instrument;
 struct ReliabilityState {
     inflight: HashMap<u32, (Vec<u8>, Option<oneshot::Sender<Result<(), eyre::Report>>>)>, // list of inflight seqs
     retx_tracker: BTreeMap<Instant, u32>,
+    last_timeout: Option<Instant>,
     pending_payload: VecDeque<(u32, Vec<u8>)>, // payloads we have received that are waiting for a recv() call
 }
 
-#[derive(Default)]
 pub struct Reliability<C> {
+    timeout: Duration,
     inner: Arc<C>,
     state: Arc<RwLock<ReliabilityState>>,
+}
+
+impl<C: Default> Default for Reliability<C> {
+    fn default() -> Self {
+        Self {
+            timeout: Duration::from_millis(100),
+            inner: Default::default(),
+            state: Default::default(),
+        }
+    }
 }
 
 impl<Cx> Reliability<Cx> {
     pub fn with_context<C>(self, cx: C) -> Reliability<C> {
         Reliability {
+            timeout: self.timeout,
             inner: Arc::new(cx),
             state: Default::default(),
         }
+    }
+
+    pub fn set_timeout(&mut self, to: Duration) -> &mut Self {
+        self.timeout = to;
+        self
     }
 }
 
 impl<C> Clone for Reliability<C> {
     fn clone(&self) -> Self {
         Reliability {
+            timeout: self.timeout,
             inner: Arc::clone(&self.inner),
             state: Arc::clone(&self.state),
         }
@@ -61,31 +79,78 @@ where
 
         let seq = data.0;
 
-        // TODO check/arm timer
-        // TODO retx on timer expiry
-
         let mut state = self.state.clone();
         let mut inner = self.inner.clone();
+        let timeout = self.timeout;
         Box::pin(
             async move {
-                let (s, r) = oneshot::channel();
+                let (s, mut r) = oneshot::channel();
                 let (seq, data) = data;
                 let buf = {
                     let mut state = state.write().await;
                     let mut buf = seq.to_be_bytes().to_vec();
                     buf.extend(&data);
-                    state.inflight.insert(seq, (data, Some(s)));
+                    state.inflight.insert(seq, (buf.clone(), Some(s)));
                     state.retx_tracker.insert(Instant::now(), seq);
                     trace!(inflight = state.inflight.len(), seq = ?seq, "sent");
                     buf
                 };
 
                 inner.send(buf).await?;
+                let state1 = Arc::clone(&state);
+                let inner1 = Arc::clone(&inner);
                 futures_util::future::select(
-                    Box::pin(async move {
-                        r.await??;
-                        Ok::<_, eyre::Report>(())
-                    }),
+                    Box::pin(
+                        async move {
+                            let state = state1;
+                            let inner = inner1;
+                            loop {
+                                tokio::select!(
+                                    done = &mut r => {
+                                        done??;
+                                        return Ok::<_, eyre::Report>(());
+                                    }
+                                    _ = tokio::time::delay_for(timeout) => {
+                                        // manage timeouts
+                                        let mut need_retx = false;
+                                        {
+                                            let st = state.read().await;
+                                            match st.last_timeout {
+                                                None => need_retx = true,
+                                                Some(then) if then.elapsed() > timeout => {
+                                                    // might need to retransmit things.
+                                                    need_retx = true;
+                                                }
+                                                _ => (),
+                                            }
+                                        } // end read lock on state
+
+
+                                        if need_retx {
+                                            debug!("checking retransmissions");
+                                            let mut st = state.write().await;
+
+                                            let cutoff = Instant::now() - timeout;
+                                            let mut unexpired = st.retx_tracker.split_off(&cutoff);
+                                            std::mem::swap(&mut unexpired, &mut st.retx_tracker);
+                                            let expired = unexpired;
+
+                                            for (_, seq) in expired {
+                                                if let Some((d, _)) = st.inflight.get(&seq) {
+                                                    debug!(seq = ?seq, "retransmitting");
+                                                    inner.send(d.clone()).await?;
+                                                    st.retx_tracker.insert(Instant::now(), seq);
+                                                }
+                                            }
+
+                                            st.last_timeout = Some(Instant::now());
+                                        }
+                                    }
+                                );
+                            }
+                        }
+                        .instrument(tracing::debug_span!("send_retx_loop")),
+                    ),
                     Box::pin(
                         async move {
                             loop {
@@ -185,6 +250,37 @@ mod test {
     use tracing::{debug, info};
     use tracing_futures::Instrument;
 
+    async fn do_transmit<C>(
+        snd_ch: Reliability<C>,
+        rcv_ch: Reliability<C>,
+        msgs: Vec<(u32, Vec<u8>)>,
+    ) where
+        C: Chunnel<Data = Vec<u8>> + Send + Sync + 'static,
+    {
+        // recv side
+        tokio::spawn(
+            async move {
+                info!("starting receiver");
+                loop {
+                    let m = rcv_ch.recv().await.unwrap();
+                    debug!(m = ?m, "rcvd");
+                }
+            }
+            .instrument(tracing::debug_span!("receiver")),
+        );
+
+        futures_util::future::join_all(msgs.into_iter().map(|m| {
+            debug!(m = ?m, "sending");
+            snd_ch.send(m)
+        }))
+        .await
+        .into_iter()
+        .collect::<Result<(), _>>()
+        .unwrap();
+
+        info!("done");
+    }
+
     #[test]
     fn no_drops() {
         let _guard = tracing_subscriber::fmt::try_init();
@@ -192,12 +288,13 @@ mod test {
 
         let mut rt = tokio::runtime::Builder::new()
             .basic_scheduler()
+            .enable_time()
             .build()
             .unwrap();
 
         rt.block_on(
             async move {
-                let mut t = Chan::new();
+                let mut t = Chan::default();
                 let mut rcv = t.listen(()).await;
                 let rcv_cn = rcv.next().await.unwrap();
                 let rcv_ch = Reliability::<()>::default().with_context(rcv_cn);
@@ -205,30 +302,43 @@ mod test {
                 let snd = t.connect(()).await;
                 let snd_ch = Reliability::<()>::default().with_context(snd);
 
-                // recv side
-                tokio::spawn(
-                    async move {
-                        info!("starting receiver");
-                        loop {
-                            let m = rcv_ch.recv().await.unwrap();
-                            debug!(m = ?m, "rcvd");
-                        }
-                    }
-                    .instrument(tracing::debug_span!("receiver")),
-                );
-
-                futures_util::future::join_all(msgs.into_iter().map(|m| {
-                    debug!(m = ?m, "sending");
-                    snd_ch.send(m)
-                }))
-                .await
-                .into_iter()
-                .collect::<Result<(), _>>()
-                .unwrap();
-
-                info!("done");
+                do_transmit(snd_ch, rcv_ch, msgs).await;
             }
             .instrument(tracing::info_span!("no_drops")),
+        );
+    }
+
+    #[test]
+    fn drop_2() {
+        let _guard = tracing_subscriber::fmt::try_init();
+        let msgs = vec![(0, vec![0u8; 10]), (1, vec![1u8; 10]), (2, vec![2u8; 10])];
+
+        let mut rt = tokio::runtime::Builder::new()
+            .basic_scheduler()
+            .enable_time()
+            .build()
+            .unwrap();
+
+        use std::sync::{atomic::AtomicUsize, Arc};
+
+        rt.block_on(
+            async move {
+                let mut t = Chan::default();
+                let ctr: Arc<AtomicUsize> = Arc::new(AtomicUsize::new(0));
+                t.lossy(move || {
+                    let c = ctr.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
+                    c == 2
+                });
+                let mut rcv = t.listen(()).await;
+                let rcv_cn = rcv.next().await.unwrap();
+                let rcv_ch = Reliability::<()>::default().with_context(rcv_cn);
+
+                let snd = t.connect(()).await;
+                let snd_ch = Reliability::<()>::default().with_context(snd);
+
+                do_transmit(snd_ch, rcv_ch, msgs).await;
+            }
+            .instrument(tracing::info_span!("drop_2")),
         );
     }
 }
