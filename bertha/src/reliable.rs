aee0ca5b0fc9364a39a82111c171d0a3c63d5e72
Akshay Narayan <akshayn@mit.edu>
2020-07-04 16:08:01 +0000

    bertha: Log levels

--- bertha/src/reliable.rs
+++ bertha/src/reliable.rs
@@ -9,7 +9,7 @@ use std::pin::Pin;
 use std::sync::Arc;
 use tokio::sync::oneshot;
 use tokio::sync::RwLock;
-use tracing::{debug, info};
+use tracing::{debug, trace};
 use tracing_futures::Instrument;
 
 #[derive(Default)]
@@ -59,7 +59,7 @@ where
         // TODO check/arm timer
         // TODO retx on timer expiry
 
-        let state = self.state.clone();
+        let mut state = self.state.clone();
         let inner = self.inner.clone();
         Box::pin(
             async move {
@@ -70,9 +70,10 @@ where
                     let mut buf = seq.to_be_bytes().to_vec();
                     buf.extend(&data);
                     state.inflight.insert(seq, (data, Some(s)));
-                    info!(inflight = state.inflight.len(), "sent");
+                    trace!(inflight = state.inflight.len(), "sent");
                     buf
                 };
+
                 inner.send(buf).await?;
                 futures_util::future::select(
                     Box::pin(async move {
@@ -81,42 +82,12 @@ where
                     }),
                     Box::pin(
                         async move {
-                            loop {
-                                let mut seq = match inner.recv().await {
-                                    Ok(s) => s,
-                                    Err(e) => {
-                                        debug!(err = ?e, "Error from inner chunnel");
-                                        continue;
-                                    }
-                                };
-                                // pop off the seqno
-                                let data = seq.split_off(4);
-                                let seq = u32::from_be_bytes(seq[0..4].try_into().unwrap());
-
-                                if data.is_empty() {
-                                    // it was an ack
-                                    let mut st = state.write().await;
-                                    info!(seq = ?seq, "got ack");
-                                    if let Some((_, Some(s))) = st.inflight.remove(&seq) {
-                                        s.send(Ok(())).unwrap_or_else(|_| {
-                                            debug!(seq = ?seq, "discarding oneshot send");
-                                        });
-                                    }
-                                } else {
-                                    // send an ack
-                                    if let Err(e) = inner.send(seq.to_be_bytes().to_vec()).await {
-                                        debug!(err = ?e, "Error from inner chunnel");
-                                    }
-
-                                    info!(seq = ?seq, len=data.len(), "got payload, sent ack");
-                                    {
-                                        let mut st = state.write().await;
-                                        st.pending_payload.push_back((seq, data));
-                                    }
-                                }
-                            }
+                            let r = do_recv(inner, &mut state).await?;
+                            let mut st = state.write().await;
+                            st.pending_payload.push_back(r);
+                            Ok::<_, eyre::Report>(())
                         }
-                        .instrument(tracing::info_span!("send_select_recv", seq=?seq)),
+                        .instrument(tracing::debug_span!("send_select_recv", seq=?seq)),
                     ),
                 )
                 .await
@@ -130,8 +101,8 @@ where
     fn recv(
         &self,
     ) -> Pin<Box<dyn Future<Output = Result<Self::Data, eyre::Report>> + Send + Sync>> {
+        let mut state = self.state.clone();
         let inner = self.inner.clone();
-        let state = self.state.clone();
 
         Box::pin(
             async move {
@@ -144,32 +115,7 @@ where
                     }
                 }
 
-                loop {
-                    info!("call inner recv");
-                    let mut seq = inner.recv().await?;
-                    // pop off the seqno
-                    let data = seq.split_off(4);
-                    let seq = u32::from_be_bytes(seq[0..4].try_into().unwrap());
-
-                    if data.is_empty() {
-                        // it was an ack
-                        let mut st = state.write().await;
-                        info!(seq = ?seq, "got ack");
-                        if let Some((_, Some(s))) = st.inflight.remove(&seq) {
-                            s.send(Ok(())).unwrap_or_else(|_| {
-                                debug!(seq = ?seq, "discarding oneshot send");
-                            });
-                        }
-
-                        continue;
-                    } else {
-                        // send an ack
-                        inner.send(seq.to_be_bytes().to_vec()).await?;
-                        info!(seq = ?seq, len=data.len(), "got payload, sent ack");
-                    }
-
-                    return Ok((seq, data));
-                }
+                do_recv(inner, &mut state).await
             }
             .instrument(tracing::debug_span!("recv")),
         )
@@ -188,19 +134,53 @@ where
     }
 }
 
+async fn do_recv<C>(
+    inner: Arc<C>,
+    state: &mut Arc<RwLock<ReliabilityState>>,
+) -> Result<(u32, Vec<u8>), eyre::Report>
+where
+    C: Chunnel<Data = Vec<u8>> + Send + Sync + 'static,
+{
+    loop {
+        trace!("call inner recv");
+        let mut seq = inner.recv().await?;
+        // pop off the seqno
+        let data = seq.split_off(4);
+        let seq = u32::from_be_bytes(seq[0..4].try_into().unwrap());
+
+        if data.is_empty() {
+            // it was an ack
+            let mut st = state.write().await;
+            trace!(seq = ?seq, "got ack");
+            if let Some((_, Some(s))) = st.inflight.remove(&seq) {
+                s.send(Ok(())).unwrap_or_else(|_| {
+                    debug!(seq = ?seq, "discarding oneshot send");
+                });
+            }
+
+            continue;
+        } else {
+            // send an ack
+            inner.send(seq.to_be_bytes().to_vec()).await?;
+            trace!(seq = ?seq, len=data.len(), "got payload, sent ack");
+        }
+
+        return Ok((seq, data));
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::Reliability;
     use crate::chan_transport::Chan;
     use crate::{Chunnel, Connector};
     use futures_util::StreamExt;
-    use tracing::info;
+    use tracing::{debug, info};
     use tracing_futures::Instrument;
 
     #[test]
     fn no_drops() {
-        let _guard = tracing_subscriber::fmt().try_init();
-
+        let _guard = tracing_subscriber::fmt::try_init();
         let msgs = vec![(0, vec![0u8; 10]), (1, vec![1u8; 10]), (2, vec![2u8; 10])];
 
         let mut rt = tokio::runtime::Builder::new()
@@ -224,14 +204,14 @@ mod test {
                         info!("starting receiver");
                         loop {
                             let m = rcv_ch.recv().await.unwrap();
-                            info!(m = ?m, "rcvd");
+                            debug!(m = ?m, "rcvd");
                         }
                     }
-                    .instrument(tracing::info_span!("receiver")),
+                    .instrument(tracing::debug_span!("receiver")),
                 );
 
                 futures_util::future::join_all(msgs.into_iter().map(|m| {
-                    info!(m = ?m, "sending");
+                    debug!(m = ?m, "sending");
                     snd_ch.send(m)
                 }))
                 .await
