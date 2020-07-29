7fed380d347d27666c6fa1a3c5509d4052a01331
Akshay Narayan <akshayn@mit.edu>
2020-07-29 01:41:27 +0000

    bertha: NegotiatorChunnel-2 implementation attempt

--- bertha/src/lib.rs
+++ bertha/src/lib.rs
@@ -58,6 +58,133 @@ pub trait ChunnelConnection {
     ) -> Pin<Box<dyn Future<Output = Result<Self::Data, eyre::Report>> + Send + 'static>>;
 }
 
+pub struct NegotiatorChunnel<T1, T2>(T1, T2);
+
+impl<A, E, T1, C1, T2, C2, D> ChunnelListener for NegotiatorChunnel<T1, T2>
+where
+    T1: ChunnelListener<Addr = A, Error = E, Connection = C1>,
+    T2: ChunnelListener<Addr = A, Error = E, Connection = C2>,
+    C1: ChunnelConnection<Data = D> + 'static,
+    C2: ChunnelConnection<Data = D> + 'static,
+    E: Send + Sync + 'static,
+{
+    type Addr = A;
+    type Connection = Either<T1::Connection, T2::Connection>;
+    type Error = E;
+    type Future = Pin<Box<dyn Future<Output = Result<Self::Stream, Self::Error>> + Send + 'static>>;
+    type Stream =
+        Pin<Box<dyn Stream<Item = Result<Self::Connection, Self::Error>> + Send + 'static>>;
+
+    fn listen(&mut self, a: Self::Addr) -> Self::Future {
+        let use_t1 = match (T1::scope(), T2::scope()) {
+            (x, y) if x == y => {
+                if T1::implementation_priority() >= T2::implementation_priority() {
+                    true
+                } else {
+                    false
+                }
+            }
+            (Scope::Application, _) => true,
+            (_, Scope::Application) => false,
+            (Scope::Host, _) => true,
+            (_, Scope::Host) => false,
+            (Scope::Local, _) => true,
+            (_, Scope::Local) => false,
+            (Scope::Global, _) => true,
+        };
+
+        use futures_util::TryStreamExt;
+
+        if use_t1 {
+            let fut = self.0.listen(a);
+            Box::pin(async move {
+                Ok::<_, E>(Box::pin(fut.await?.map_ok(|c| Either::Left(c)))
+                    as Pin<
+                        Box<
+                            dyn Stream<Item = Result<Self::Connection, Self::Error>>
+                                + Send
+                                + 'static,
+                        >,
+                    >)
+            }) as _
+        } else {
+            let fut = self.1.listen(a);
+            Box::pin(async move {
+                Ok::<_, E>(Box::pin(fut.await?.map_ok(|c| Either::Right(c)))
+                    as Pin<
+                        Box<
+                            dyn Stream<Item = Result<Self::Connection, Self::Error>>
+                                + Send
+                                + 'static,
+                        >,
+                    >)
+            }) as _
+        }
+    }
+
+    fn scope() -> Scope {
+        unimplemented!()
+    }
+    fn endedness() -> Endedness {
+        unimplemented!()
+    }
+    fn implementation_priority() -> usize {
+        unimplemented!()
+    }
+}
+
+impl<A, E, T1, C1, T2, C2, D> ChunnelConnector for NegotiatorChunnel<T1, T2>
+where
+    T1: ChunnelConnector<Addr = A, Error = E, Connection = C1>,
+    T2: ChunnelConnector<Addr = A, Error = E, Connection = C2>,
+    C1: ChunnelConnection<Data = D> + 'static,
+    C2: ChunnelConnection<Data = D> + 'static,
+    E: Send + Sync + 'static,
+{
+    type Addr = A;
+    type Connection = Either<T1::Connection, T2::Connection>;
+    type Error = E;
+    type Future =
+        Pin<Box<dyn Future<Output = Result<Self::Connection, Self::Error>> + Send + 'static>>;
+
+    fn connect(&mut self, a: Self::Addr) -> Self::Future {
+        let use_t1 = match (T1::scope(), T2::scope()) {
+            (x, y) if x == y => {
+                if T1::implementation_priority() >= T2::implementation_priority() {
+                    true
+                } else {
+                    false
+                }
+            }
+            (Scope::Application, _) => true,
+            (_, Scope::Application) => false,
+            (Scope::Host, _) => true,
+            (_, Scope::Host) => false,
+            (Scope::Local, _) => true,
+            (_, Scope::Local) => false,
+            (Scope::Global, _) => true,
+        };
+
+        if use_t1 {
+            let fut = self.0.connect(a);
+            Box::pin(async move { Ok(Either::Left(fut.await?)) }) as _
+        } else {
+            let fut = self.1.connect(a);
+            Box::pin(async move { Ok(Either::Right(fut.await?)) }) as _
+        }
+    }
+
+    fn scope() -> Scope {
+        unimplemented!()
+    }
+    fn endedness() -> Endedness {
+        unimplemented!()
+    }
+    fn implementation_priority() -> usize {
+        unimplemented!()
+    }
+}
+
 /// A standard way to access the downstream chunnel.
 pub trait Context {
     type ChunnelType;
@@ -170,9 +297,43 @@ impl IpPort for std::net::SocketAddr {
     }
 }
 
+#[pin_project::pin_project(project = EitherProj)]
 pub enum Either<A, B> {
-    Left(A),
-    Right(B),
+    Left(#[pin] A),
+    Right(#[pin] B),
+}
+
+impl<A, B, O> Future for Either<A, B>
+where
+    A: Future<Output = O>,
+    B: Future<Output = O>,
+{
+    type Output = O;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut std::task::Context) -> std::task::Poll<Self::Output> {
+        match self.project() {
+            EitherProj::Left(a) => a.poll(cx),
+            EitherProj::Right(a) => a.poll(cx),
+        }
+    }
+}
+
+impl<A, B, I> Stream for Either<A, B>
+where
+    A: Stream<Item = I>,
+    B: Stream<Item = I>,
+{
+    type Item = I;
+
+    fn poll_next(
+        self: Pin<&mut Self>,
+        cx: &mut std::task::Context,
+    ) -> std::task::Poll<Option<Self::Item>> {
+        match self.project() {
+            EitherProj::Left(a) => a.poll_next(cx),
+            EitherProj::Right(a) => a.poll_next(cx),
+        }
+    }
 }
 
 impl<A, B, D> ChunnelConnection for Either<A, B>
@@ -567,6 +728,7 @@ where
 }
 
 /// Where the Chunnel implementation allows functionality to be implemented.
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Scope {
     /// Must be inside the application.
     Application,
