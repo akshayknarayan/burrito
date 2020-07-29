34830805000d12d2db522db74caffb638fe2e002
Akshay Narayan <akshayn@mit.edu>
2020-07-29 17:40:58 +0000

    bertha: Reorganize into modules

---
+++ bertha/src/either.rs
@@ -0,0 +1,73 @@
+//! The `Either` type implements traits where both variants implement the trait with the same
+//! output type.
+
+use super::ChunnelConnection;
+use futures_util::stream::Stream;
+use std::future::Future;
+use std::pin::Pin;
+
+#[pin_project::pin_project(project = EitherProj)]
+pub enum Either<A, B> {
+    Left(#[pin] A),
+    Right(#[pin] B),
+}
+
+impl<A, B, O> Future for Either<A, B>
+where
+    A: Future<Output = O>,
+    B: Future<Output = O>,
+{
+    type Output = O;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut std::task::Context) -> std::task::Poll<Self::Output> {
+        match self.project() {
+            EitherProj::Left(a) => a.poll(cx),
+            EitherProj::Right(a) => a.poll(cx),
+        }
+    }
+}
+
+impl<A, B, I> Stream for Either<A, B>
+where
+    A: Stream<Item = I>,
+    B: Stream<Item = I>,
+{
+    type Item = I;
+
+    fn poll_next(
+        self: Pin<&mut Self>,
+        cx: &mut std::task::Context,
+    ) -> std::task::Poll<Option<Self::Item>> {
+        match self.project() {
+            EitherProj::Left(a) => a.poll_next(cx),
+            EitherProj::Right(a) => a.poll_next(cx),
+        }
+    }
+}
+
+impl<A, B, D> ChunnelConnection for Either<A, B>
+where
+    A: ChunnelConnection<Data = D>,
+    B: ChunnelConnection<Data = D>,
+{
+    type Data = D;
+
+    fn send(
+        &self,
+        data: Self::Data,
+    ) -> Pin<Box<dyn Future<Output = Result<(), eyre::Report>> + Send + 'static>> {
+        match self {
+            Either::Left(a) => a.send(data),
+            Either::Right(b) => b.send(data),
+        }
+    }
+
+    fn recv(
+        &self,
+    ) -> Pin<Box<dyn Future<Output = Result<Self::Data, eyre::Report>> + Send + 'static>> {
+        match self {
+            Either::Left(a) => a.recv(),
+            Either::Right(b) => b.recv(),
+        }
+    }
+}
---
+++ bertha/src/inherit.rs
@@ -0,0 +1,102 @@
+//! Helper traits to avoid re-writing common Chunnel implementation code for simple Chunnels.
+
+use super::{ChunnelConnection, ChunnelConnector, ChunnelListener, Endedness, Scope};
+use futures_util::stream::Stream;
+use std::future::Future;
+use std::pin::Pin;
+
+/// A standard way to access the downstream chunnel.
+pub trait Context {
+    type ChunnelType;
+    fn context(&self) -> &Self::ChunnelType;
+    fn context_mut(&mut self) -> &mut Self::ChunnelType;
+}
+
+/// A simpler trait to implement for traits with simpler (or no) Chunnel setup.
+pub trait InheritChunnel<CxConn: ChunnelConnection>: Context {
+    type Connection: ChunnelConnection;
+    type Config: Clone + Send + Sync + 'static;
+
+    fn get_config(&mut self) -> Self::Config;
+    fn make_connection(cx: CxConn, cfg: Self::Config) -> Self::Connection;
+}
+
+impl<C> ChunnelListener for C
+where
+    C: Context,
+    <C as Context>::ChunnelType: ChunnelListener,
+    <<C as Context>::ChunnelType as ChunnelListener>::Connection: 'static,
+    C: InheritChunnel<<<C as Context>::ChunnelType as ChunnelListener>::Connection>,
+    <C as InheritChunnel<<<C as Context>::ChunnelType as ChunnelListener>::Connection>>::Connection:
+        'static,
+{
+    type Addr = <<C as Context>::ChunnelType as ChunnelListener>::Addr;
+    type Connection = C::Connection;
+    type Error = <<C as Context>::ChunnelType as ChunnelListener>::Error;
+    type Future = Pin<Box<dyn Future<Output = Result<Self::Stream, Self::Error>> + Send + 'static>>;
+    type Stream =
+        Pin<Box<dyn Stream<Item = Result<Self::Connection, Self::Error>> + Send + 'static>>;
+
+    fn listen(&mut self, a: Self::Addr) -> Self::Future {
+        use futures_util::StreamExt;
+
+        let f = self.context_mut().listen(a);
+        let cfg = self.get_config();
+        Box::pin(async move {
+            let cfg = cfg;
+            let conn_stream = f.await?;
+            Ok(Box::pin(conn_stream.map(move |conn| {
+                let cfg = cfg.clone();
+                Ok(C::make_connection(conn?, cfg))
+            })) as _)
+        })
+    }
+
+    fn scope() -> Scope {
+        <C as Context>::ChunnelType::scope()
+    }
+
+    fn endedness() -> Endedness {
+        <C as Context>::ChunnelType::endedness()
+    }
+
+    fn implementation_priority() -> usize {
+        <C as Context>::ChunnelType::implementation_priority()
+    }
+    // fn resource_requirements(&self) -> ?;
+}
+
+impl<C> ChunnelConnector for C
+where
+    C: Context,
+    <C as Context>::ChunnelType: ChunnelConnector,
+    <<C as Context>::ChunnelType as ChunnelConnector>::Connection: 'static,
+    C: InheritChunnel<<<C as Context>::ChunnelType as ChunnelConnector>::Connection>,
+    <C as InheritChunnel<<<C as Context>::ChunnelType as ChunnelConnector>::Connection>>::Connection:
+        'static,
+{
+    type Addr = <<C as Context>::ChunnelType as ChunnelConnector>::Addr;
+    type Connection = C::Connection;
+    type Error = <<C as Context>::ChunnelType as ChunnelConnector>::Error;
+    type Future =
+        Pin<Box<dyn Future<Output = Result<Self::Connection, Self::Error>> + Send + 'static>>;
+
+    fn connect(&mut self, a: Self::Addr) -> Self::Future {
+        let f = self.context_mut().connect(a);
+        let cfg = self.get_config();
+        Box::pin(async move { Ok(C::make_connection(f.await?, cfg)) })
+    }
+
+    fn scope() -> Scope {
+        <C as Context>::ChunnelType::scope()
+    }
+
+    fn endedness() -> Endedness {
+        <C as Context>::ChunnelType::endedness()
+    }
+
+    fn implementation_priority() -> usize {
+        <C as Context>::ChunnelType::implementation_priority()
+    }
+    // fn resource_requirements(&self) -> ?;
+}
