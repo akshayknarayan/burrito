50d8aa0c70d72f38b41baa883ab9350714439c1c
Akshay Narayan <akshayn@mit.edu>
2020-12-02 18:34:00 +0000

    shenango: Remove second blocking channel call

--- shenango-chunnel/src/lib.rs
+++ shenango-chunnel/src/lib.rs
@@ -13,6 +13,7 @@ use std::sync::{
     Arc,
 };
 use tokio::sync::{mpsc, Mutex};
+use tracing::{debug, trace};
 
 static SHENANGO_RUNTIME_INIT: AtomicBool = AtomicBool::new(false);
 
@@ -28,6 +29,74 @@ enum NewConn {
     },
 }
 
+impl NewConn {
+    fn start(self) {
+        use NewConn::*;
+        let (sk, incoming, outgoing) = match self {
+            Listen {
+                addr,
+                incoming,
+                outgoing,
+            } => (
+                Arc::new(
+                    shenango::udp::UdpConnection::listen(addr)
+                        .wrap_err("Failed to make shenango udp socket")
+                        .expect("make udp conn"),
+                ),
+                incoming,
+                outgoing,
+            ),
+            Dial { incoming, outgoing } => {
+                let laddr = SocketAddrV4::new(std::net::Ipv4Addr::new(0, 0, 0, 0), 0);
+                (
+                    Arc::new(
+                        shenango::udp::UdpConnection::listen(laddr)
+                            .wrap_err("Failed to make shenango udp socket")
+                            .expect("make udp conn"),
+                    ),
+                    incoming,
+                    outgoing,
+                )
+            }
+        };
+
+        let laddr = sk.local_addr();
+
+        let rsk = Arc::clone(&sk);
+        // receive
+        shenango::thread::spawn(move || {
+            let mut buf = [0u8; 1024];
+            loop {
+                let (read_len, from_addr) = rsk
+                    .read_from(&mut buf)
+                    .wrap_err("shenango read_from")
+                    .unwrap();
+                trace!(sk=?laddr, "recv");
+                incoming
+                    .send((from_addr, buf[..read_len].to_vec()))
+                    .unwrap();
+            }
+        });
+
+        // send
+        shenango::thread::spawn(move || loop {
+            match outgoing.try_recv() {
+                Ok((a, data)) => {
+                    trace!(sk=?laddr, "send");
+                    sk.write_to(&data, a).wrap_err("shenango write_to").unwrap();
+                }
+                Err(crossbeam::channel::TryRecvError::Empty) => {
+                    shenango::thread::thread_yield();
+                }
+                Err(crossbeam::channel::TryRecvError::Disconnected) => {
+                    debug!(sk=?laddr, "send thread exiting");
+                    break;
+                }
+            }
+        });
+    }
+}
+
 // everything here blocks.
 // On success, this won't return while the ShenangoUdpSkChunnel still lives.
 fn shenango_runtime_start(
@@ -37,66 +106,18 @@ fn shenango_runtime_start(
     SHENANGO_RUNTIME_INIT
         .compare_exchange(false, true, Ordering::SeqCst, Ordering::SeqCst)
         .map_err(|_| eyre!("Can only start shenango runtime once"))?;
-    shenango::runtime_init(shenango_config.to_str().unwrap().to_owned(), move || {
-        while let Ok(conn) = conns.recv() {
-            use NewConn::*;
-            let (sk, incoming, outgoing) = match conn {
-                Listen {
-                    addr,
-                    incoming,
-                    outgoing,
-                } => (
-                    Arc::new(
-                        shenango::udp::UdpConnection::listen(addr)
-                            .wrap_err("Failed to make shenango udp socket")
-                            .expect("make udp conn"),
-                    ),
-                    incoming,
-                    outgoing,
-                ),
-                Dial { incoming, outgoing } => {
-                    let laddr = SocketAddrV4::new(std::net::Ipv4Addr::new(0, 0, 0, 0), 0);
-                    (
-                        Arc::new(
-                            shenango::udp::UdpConnection::listen(laddr)
-                                .wrap_err("Failed to make shenango udp socket")
-                                .expect("make udp conn"),
-                        ),
-                        incoming,
-                        outgoing,
-                    )
-                }
-            };
-
-            let rsk = Arc::clone(&sk);
-            // receive
-            shenango::thread::spawn(move || {
-                let mut buf = [0u8; 1024];
-                loop {
-                    let (read_len, from_addr) = rsk
-                        .read_from(&mut buf)
-                        .wrap_err("shenango read_from")
-                        .unwrap();
-                    incoming
-                        .send((from_addr, buf[..read_len].to_vec()))
-                        .unwrap();
-                }
-            });
-
-            // send
-            shenango::thread::spawn(move || loop {
-                match outgoing.try_recv() {
-                    Ok((a, data)) => {
-                        sk.write_to(&data, a).wrap_err("shenango write_to").unwrap();
-                    }
-                    Err(crossbeam::channel::TryRecvError::Empty) => {
-                        shenango::thread::thread_yield();
-                    }
-                    Err(crossbeam::channel::TryRecvError::Disconnected) => {
-                        break;
-                    }
-                }
-            });
+    shenango::runtime_init(shenango_config.to_str().unwrap().to_owned(), move || loop {
+        match conns.try_recv() {
+            Ok(conn) => {
+                conn.start();
+            }
+            Err(crossbeam::channel::TryRecvError::Empty) => {
+                shenango::thread::thread_yield();
+            }
+            Err(crossbeam::channel::TryRecvError::Disconnected) => {
+                debug!("main thread exiting");
+                break;
+            }
         }
     })
     .map_err(|i| eyre!("shenango runtime error: {:?}", i))
