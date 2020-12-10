3e4db51745ae1e833802660d63c16785e9706b55
Akshay Narayan <akshayn@mit.edu>
2020-12-10 17:08:30 +0000

    kvstore/reliability: Test

--- kvstore/src/reliability.rs
+++ kvstore/src/reliability.rs
@@ -10,14 +10,20 @@ use std::pin::Pin;
 use std::sync::Arc;
 use std::time::Duration;
 use tokio::sync::{mpsc, oneshot, Mutex};
+use tracing::{trace, trace_span};
+use tracing_futures::Instrument;
 
 /// Reliable transmission of request/response pairs.
 ///
 /// Assumptions:
 /// 1. Data segments have an id (the `MsgId` trait)
-/// 2. Each request will generate a single response, and that response data segment will have a matching id.
+/// 2. Each request will generate a single response, and that response data segment will have a
+///    matching id.
 ///
-/// These assumptions allow us to forego seqno generation and sending acks.
+/// These assumptions allow us to forego seqno generation and explicit acks.  As a result, however,
+/// this will only work on the client side, since we assume that `send`s and `recv`s are paired and
+/// that the `send` is first. Waiting on a `recv` without a prior `send` will block forever.
+#[derive(Debug, Clone)]
 pub struct KvReliabilityChunnel {
     timeout: Duration,
 }
@@ -129,6 +135,7 @@ where
         self.sends.insert(msg_id, s);
         let sends = Arc::clone(&self.sends);
         Box::pin(async move {
+            trace!("start");
             inner.send(data.clone()).await?;
             loop {
                 tokio::select!(
@@ -137,6 +144,7 @@ where
 
                         // careful about blocking here, could deadlock
                         let recv_msg_id = resp.1.id();
+                        trace!(id = ?recv_msg_id, "recv");
                         signal_recv.send(resp).await.map_err(|_| ()).expect("receiver won't hang up");
 
                         let send_ch = if let Some(s) = sends.remove(&msg_id) {
@@ -150,14 +158,16 @@ where
                         }
                     }
                     _ = &mut r => {
+                        trace!("signaled");
                         return Ok(());
                     }
                     _ = tokio::time::sleep(to) => {
+                        trace!("retx");
                         inner.send(data.clone()).await?;
                     }
                 );
             }
-        })
+        }.instrument(trace_span!("send", ?msg_id)))
     }
 
     fn recv(
@@ -166,5 +176,101 @@ where
         // return next signaled recv
         let r = Arc::clone(&self.recvs);
         Box::pin(async move { Ok(r.lock().await.recv().await.unwrap()) })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::KvReliabilityChunnel;
+    use bertha::{
+        chan_transport::Chan, util::ProjectLeft, ChunnelConnection, ChunnelConnector,
+        ChunnelListener, Client, CxList, Serve,
+    };
+    use futures_util::StreamExt;
+    use tracing::{debug, info};
+    use tracing_error::ErrorLayer;
+    use tracing_futures::Instrument;
+    use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+    #[derive(Clone, Copy, Debug)]
+    struct Msg {
+        field: usize,
+    }
+
+    impl bertha::util::MsgId for Msg {
+        fn id(&self) -> usize {
+            self.field
+        }
+    }
+
+    #[test]
+    fn drop_2() {
+        let subscriber = tracing_subscriber::registry()
+            .with(tracing_subscriber::fmt::layer())
+            .with(tracing_subscriber::EnvFilter::from_default_env())
+            .with(ErrorLayer::default());
+        let _guard = subscriber.set_default();
+        color_eyre::install().unwrap_or_else(|_| ());
+        let msgs: Vec<_> = (0..10).map(|field| ((), Msg { field })).collect();
+
+        let rt = tokio::runtime::Builder::new_current_thread()
+            .enable_time()
+            .build()
+            .unwrap();
+
+        use std::sync::{atomic::AtomicUsize, Arc};
+
+        rt.block_on(
+            async move {
+                let mut t = Chan::default();
+                let ctr: Arc<AtomicUsize> = Arc::new(AtomicUsize::new(0));
+                t.link_conditions(move |x| {
+                    let c = ctr.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
+                    if c != 2 {
+                        x
+                    } else {
+                        None
+                    }
+                });
+
+                let (mut srv, mut cln) = t.split();
+
+                let mut l = ProjectLeft::from(());
+                let rcv_st = srv.listen(()).await.unwrap();
+                let mut rcv_st = l.serve(rcv_st).await.unwrap();
+                let rcv = rcv_st.next().await.unwrap().unwrap();
+
+                let mut l =
+                    CxList::from(KvReliabilityChunnel::default()).wrap(ProjectLeft::from(()));
+                let cln = cln.connect(()).await.unwrap();
+                let snd = l.connect_wrap(cln).await.unwrap();
+
+                // recv side
+                tokio::spawn(
+                    async move {
+                        info!("starting receiver");
+                        loop {
+                            let m: ((), Msg) = rcv.recv().await.unwrap();
+                            debug!(?m, "rcvd");
+                            rcv.send(m).await.unwrap();
+                        }
+                    }
+                    .instrument(tracing::info_span!("receiver")),
+                );
+
+                tokio::time::sleep(std::time::Duration::from_millis(100)).await;
+
+                for m in msgs {
+                    debug!(?m, "sending");
+                    snd.send(m).await.unwrap();
+                    debug!(?m, "getting response");
+                    let m: ((), Msg) = snd.recv().await.unwrap();
+                    debug!(?m, "done");
+                }
+
+                info!("done");
+            }
+            .instrument(tracing::info_span!("drop_2")),
+        );
     }
 }
