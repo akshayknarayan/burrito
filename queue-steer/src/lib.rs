769ecb4ab0ca8cd6df98d77847bc09d19c87fdff
Akshay Narayan <akshayn@mit.edu>
2021-03-12 06:00:57 +0000

    queue-steer: Base multiplexing queue chunnel

--- queue-steer/src/lib.rs
+++ queue-steer/src/lib.rs
@@ -1,12 +1,19 @@
 //! Steer messages to the right queue.
 
+use az_queues::AzStorageQueueChunnel;
+use bertha::{Chunnel, ChunnelConnection, Negotiate};
 use color_eyre::eyre::{bail, eyre, Report};
+use futures_util::future::{ready, Ready};
+use gcp_pubsub::{PubSubAddr, PubSubChunnel};
+use sqs::{SqsAddr, SqsChunnel};
+use std::future::Future;
+use std::pin::Pin;
 
 #[derive(Debug, Clone)]
 pub enum QueueAddr {
-    Aws(String),
+    Aws(SqsAddr),
     Azure(String),
-    Gcp(String),
+    Gcp(PubSubAddr),
 }
 
 impl std::str::FromStr for QueueAddr {
@@ -16,12 +23,18 @@ impl std::str::FromStr for QueueAddr {
             .find(':')
             .ok_or_else(|| eyre!("Malformed QueueAddr: {:?}", s))?;
         let sp = [&s[0..idx], &s[idx + 1..]];
-        match sp {
-            ["aws", g] | ["AWS", g] => Ok(QueueAddr::Aws(g.to_string())),
-            ["az", g] | ["Azure", g] => Ok(QueueAddr::Azure(g.to_string())),
-            ["gcp", g] | ["GCP", g] => Ok(QueueAddr::Gcp(g.to_string())),
+        Ok(match sp {
+            ["aws", g] | ["AWS", g] => QueueAddr::Aws(SqsAddr {
+                queue_id: g.to_string(),
+                group: None,
+            }),
+            ["az", g] | ["Azure", g] => QueueAddr::Azure(g.to_string()),
+            ["gcp", g] | ["GCP", g] => QueueAddr::Gcp(PubSubAddr {
+                topic_id: g.to_string(),
+                group: None,
+            }),
             _ => bail!("Unkown addr {:?} -> {:?}", s, sp),
-        }
+        })
     }
 }
 
@@ -48,5 +61,188 @@ impl SetGroup for sqs::SqsAddr {
 impl SetGroup for gcp_pubsub::PubSubAddr {
     fn set_group(&mut self, group: String) {
         self.group = Some(group);
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, serde::Serialize, serde::Deserialize)]
+pub enum MessageQueueOrdering {
+    BestEffort,
+    Ordered,
+    GroupOrdered { num_groups: usize },
+    ArbitraryDependencies(String),
+}
+
+#[derive(
+    Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, serde::Serialize, serde::Deserialize,
+)]
+pub enum MessageQueueReliability {
+    AtLeastOnce,
+    AtMostOnce,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, serde::Serialize, serde::Deserialize)]
+pub struct MessageQueueCaps {
+    ordering: MessageQueueOrdering,
+    reliability: MessageQueueReliability,
+}
+
+impl bertha::negotiate::CapabilitySet for MessageQueueCaps {
+    fn guid() -> u64 {
+        0x917be1a7e2a5c907
+    }
+
+    fn universe() -> Option<Vec<Self>> {
+        // return None to force both sides to match
+        None // TODO is this correct?
+    }
+}
+
+/// Wraps basic chunnel connections.
+///
+/// Since this is a wrapper, it can impl `Chunnel` and `Negotiate`.
+#[derive(Debug, Clone)]
+pub struct BaseQueueChunnel;
+
+impl Negotiate for BaseQueueChunnel {
+    type Capability = MessageQueueCaps;
+
+    fn guid() -> u64 {
+        0xc59e844359deaaa3
+    }
+
+    fn capabilities() -> Vec<Self::Capability> {
+        vec![MessageQueueCaps {
+            ordering: MessageQueueOrdering::BestEffort,
+            reliability: MessageQueueReliability::AtLeastOnce,
+        }]
+    }
+}
+
+impl Chunnel<SqsChunnel> for BaseQueueChunnel {
+    type Future = Ready<Result<Self::Connection, Self::Error>>;
+    type Connection = BaseQueue;
+    type Error = std::convert::Infallible;
+
+    fn connect_wrap(&mut self, cn: SqsChunnel) -> Self::Future {
+        ready(Ok(cn.into()))
+    }
+}
+
+impl Chunnel<AzStorageQueueChunnel> for BaseQueueChunnel {
+    type Future = Ready<Result<Self::Connection, Self::Error>>;
+    type Connection = BaseQueue;
+    type Error = std::convert::Infallible;
+
+    fn connect_wrap(&mut self, cn: AzStorageQueueChunnel) -> Self::Future {
+        ready(Ok(cn.into()))
+    }
+}
+
+impl Chunnel<PubSubChunnel> for BaseQueueChunnel {
+    type Future = Ready<Result<Self::Connection, Self::Error>>;
+    type Connection = BaseQueue;
+    type Error = std::convert::Infallible;
+
+    fn connect_wrap(&mut self, cn: PubSubChunnel) -> Self::Future {
+        ready(Ok(cn.into()))
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum BaseQueue {
+    Aws(SqsChunnel),
+    Azure(AzStorageQueueChunnel),
+    Gcp(PubSubChunnel),
+}
+
+impl From<SqsChunnel> for BaseQueue {
+    fn from(s: SqsChunnel) -> Self {
+        BaseQueue::Aws(s)
+    }
+}
+
+impl From<AzStorageQueueChunnel> for BaseQueue {
+    fn from(s: AzStorageQueueChunnel) -> Self {
+        BaseQueue::Azure(s)
+    }
+}
+
+impl From<PubSubChunnel> for BaseQueue {
+    fn from(s: PubSubChunnel) -> Self {
+        BaseQueue::Gcp(s)
+    }
+}
+
+impl ChunnelConnection for BaseQueue {
+    type Data = (QueueAddr, String);
+
+    fn send(
+        &self,
+        (addr, data): Self::Data,
+    ) -> Pin<Box<dyn Future<Output = Result<(), Report>> + Send + 'static>> {
+        match self {
+            BaseQueue::Aws(this) => {
+                let addr = match addr {
+                    QueueAddr::Aws(a) => a,
+                    x => {
+                        return Box::pin(ready(Err(eyre!(
+                            "Wrong address type for underlying queue: {:?}",
+                            x
+                        ))))
+                    }
+                };
+                this.send((addr, data))
+            }
+            BaseQueue::Azure(this) => {
+                let addr = match addr {
+                    QueueAddr::Azure(a) => a,
+                    x => {
+                        return Box::pin(ready(Err(eyre!(
+                            "Wrong address type for underlying queue: {:?}",
+                            x
+                        ))))
+                    }
+                };
+                this.send((addr, data))
+            }
+            BaseQueue::Gcp(this) => {
+                let addr = match addr {
+                    QueueAddr::Gcp(a) => a,
+                    x => {
+                        return Box::pin(ready(Err(eyre!(
+                            "Wrong address type for underlying queue: {:?}",
+                            x
+                        ))))
+                    }
+                };
+                this.send((addr, data))
+            }
+        }
+    }
+
+    fn recv(&self) -> Pin<Box<dyn Future<Output = Result<Self::Data, Report>> + Send + 'static>> {
+        match self {
+            BaseQueue::Aws(this) => {
+                let fut = this.recv();
+                Box::pin(async move {
+                    let (addr, data) = fut.await?;
+                    Ok((QueueAddr::Aws(addr), data))
+                })
+            }
+            BaseQueue::Azure(this) => {
+                let fut = this.recv();
+                Box::pin(async move {
+                    let (addr, data) = fut.await?;
+                    Ok((QueueAddr::Azure(addr), data))
+                })
+            }
+            BaseQueue::Gcp(this) => {
+                let fut = this.recv();
+                Box::pin(async move {
+                    let (addr, data) = fut.await?;
+                    Ok((QueueAddr::Gcp(addr), data))
+                })
+            }
+        }
     }
 }
