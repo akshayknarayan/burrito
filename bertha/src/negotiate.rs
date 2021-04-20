2c5902227ad0a9c40f993c24132f295913fb6257
Akshay Narayan <akshayn@mit.edu>
2021-04-20 18:56:26 +0000

    bertha: Increase negotiate retry timeout

--- bertha/src/negotiate.rs
+++ bertha/src/negotiate.rs
@@ -1309,7 +1309,7 @@ where
     let buf = bincode::serialize(&offer)?;
     loop {
         match time::timeout(
-            std::time::Duration::from_millis(100),
+            std::time::Duration::from_millis(2_000),
             try_once(cn, addr.clone(), buf.clone()),
         )
         .await
