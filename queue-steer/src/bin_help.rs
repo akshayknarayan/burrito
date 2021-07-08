c8a9c58fab172e305667eab49876401f5a506438
Akshay Narayan <akshayn@mit.edu>
2021-07-08 00:47:50 +0000

    queue-steer: Use send_batch/recv_batch

--- queue-steer/src/bin_help.rs
+++ queue-steer/src/bin_help.rs
@@ -80,10 +80,11 @@ where
     let recv_cns = (0..num_receivers).map(move |_| Arc::clone(&cn));
 
     let start = std::time::Instant::now();
-    let receive_handle = tokio::spawn(receive_reqs(start, num_reqs, recv_cns, false));
+    let receive_handle = tokio::spawn(receive_reqs(start, num_reqs, batch_size, recv_cns, false));
     send_reqs(
         start,
         num_reqs,
+        batch_size,
         Duration::from_millis(inter_request_ms),
         &[send_cn],
     )
@@ -111,7 +112,7 @@ where
     let recv_cns = (0..num_receivers).map(move |_| Arc::clone(&cn));
 
     let start = std::time::Instant::now();
-    let receive_handle = tokio::spawn(receive_reqs(start, num_reqs, recv_cns, false));
+    let receive_handle = tokio::spawn(receive_reqs(start, num_reqs, batch_size, recv_cns, false));
 
     // our local version of send_reqs, which uses a unique ordering key
     debug!("starting sends");
@@ -167,10 +168,11 @@ where
     let recv_cns = (0..num_receivers).map(move |_| Arc::clone(&cn));
 
     let start = std::time::Instant::now();
-    let receive_handle = tokio::spawn(receive_reqs(start, num_reqs, recv_cns, true));
+    let receive_handle = tokio::spawn(receive_reqs(start, num_reqs, batch_size, recv_cns, true));
     send_reqs(
         start,
         num_reqs,
+        batch_size,
         Duration::from_millis(inter_request_ms),
         &send_cns,
     )
@@ -209,18 +211,20 @@ impl RecvdMsg {
 async fn send_reqs(
     start: std::time::Instant,
     num_reqs: usize,
+    batch_size: usize,
     inter_request: Duration,
-    send_chunnels: &[impl ChunnelConnection<Data = Msg>],
+    send_chunnels: &[impl ChunnelConnection<Data = Msg> + Sync],
 ) -> Result<(), Report> {
     debug!("starting sends");
     let mut curr_group: usize = 0;
-    for req_num in 0..num_reqs {
-        debug!(?curr_group, ?req_num, "sending request");
+    let num_batches = num_reqs / batch_size;
+    for batch_num in 0..num_batches {
+        debug!(?curr_group, ?batch_num, "sending batch");
         send_chunnels[curr_group]
-            .send(Msg {
+            .send_batch((0..batch_size).map(|i| Msg {
                 send_time: start.elapsed(),
-                req_num,
-            })
+                req_num: batch_num * batch_size + i,
+            }))
             .await?;
         curr_group = (curr_group + 1) % send_chunnels.len();
         if inter_request.as_nanos() > 0 {
@@ -238,7 +242,8 @@ async fn send_reqs(
 async fn receive_reqs<A>(
     start: std::time::Instant,
     num_reqs: usize,
-    receive_chunnels: impl IntoIterator<Item = impl ChunnelConnection<Data = (A, Msg)>>,
+    batch_size: usize,
+    receive_chunnels: impl IntoIterator<Item = impl ChunnelConnection<Data = (A, Msg)> + Sync>,
     check_ordering: bool,
 ) -> Result<(Vec<(A, RecvdMsg)>, Duration), Report>
 where
@@ -253,16 +258,29 @@ where
             let mut msgs = Vec::new();
             let mut groups = HashMap::new();
             async move {
-                let mut f = rch.recv();
+                let mut f = rch.recv_batch(batch_size);
                 loop {
                     tokio::select!(
                         r = &mut f => {
-                            let (a, msg) = r?;
-                            msgs.push((a.clone(), RecvdMsg::from_start(start, msg)));
-                            // fetch_add returns the old value, so we re-apply the +1 locally.
-                            let tot = rcvd.fetch_add(1, Ordering::SeqCst) + 1;
-                            debug!(num = ?msg.req_num, ?tot, local = ?msgs.len(), "got msg");
-                            if msgs.len() == 1 {
+                            let recvd_batch: Result<Vec<_>, _> = r?.into_iter().map(|(a, msg)| {
+                                // check order
+                                if check_ordering {
+                                    let old_num = groups.entry(a.clone()).or_insert(msg.req_num);
+                                    if *old_num <= msg.req_num {
+                                        *old_num = msg.req_num;
+                                    } else {
+                                        let on = *old_num; // so we can end the borrow of groups
+                                        return Err(eyre!(
+                                           "Group order semantics not met in group {:?}: {:?} > {:?}: {:?}", 
+                                           a, on, msg.req_num, groups
+                                        ));
+                                    }
+                                }
+
+                                Ok((a, RecvdMsg::from_start(start, msg)))
+                            }).collect();
+                            let recvd_batch = recvd_batch?;
+                            if msgs.is_empty() {
                                 let possible_first_time = start.elapsed();
                                 match first_time.compare_exchange(
                                     0,
@@ -278,27 +296,17 @@ where
                                 }
                             }
 
-                            // check order
-                            if check_ordering {
-                                let old_num = groups.entry(a.clone()).or_insert(msg.req_num);
-                                if *old_num <= msg.req_num {
-                                    *old_num = msg.req_num;
-                                } else {
-                                    let on = *old_num;
-                                    //std::mem::drop(old_num);
-                                    return Err(eyre!(
-                                       "Group order semantics not met in group {:?}: {:?} > {:?}: {:?}", 
-                                       a, on, msg.req_num, groups
-                                    ));
-                                }
-                            }
+                            // fetch_add returns the old value, so we re-apply the increment locally.
+                            let tot = rcvd.fetch_add(recvd_batch.len(), Ordering::SeqCst) + recvd_batch.len();
+                            debug!(batch_start = ?recvd_batch[0].1.req_num, batch_end=?recvd_batch[recvd_batch.len()-1].1.req_num, ?tot, local = ?msgs.len(), "got batch");
+                            msgs.extend(recvd_batch);
 
                             if tot >= num_reqs {
                                 return Ok::<_, Report>(msgs);
                             }
 
                             // get a new recv future.
-                            f = rch.recv();
+                            f = rch.recv_batch(batch_size);
                         }
                         _ = tokio::time::sleep(Duration::from_millis(100)) => {
                             // check tot
@@ -306,13 +314,15 @@ where
                             if tot >= num_reqs {
                                 return Ok::<_, Report>(msgs);
                             }
+
+                            debug!(?tot, local=?msgs.len(), "No recv for 100ms");
                         }
                     )
 
                 }
             }
             .instrument(info_span!("receive-reqs", num=?i))
-        },
+        }
     ))
     .await?;
     let first_recv_time = Duration::from_micros(first_recv_time.load(Ordering::SeqCst));
