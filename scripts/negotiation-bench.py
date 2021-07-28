ceab3d08f2675143d2c643077104d7f490d91a02
Akshay Narayan <akshayn@mit.edu>
2021-07-28 23:11:24 +0000

    scripts: negotiation-bench scripts

---
+++ scripts/negotiation-bench.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import subprocess as sh
+import sys
+
+outdir = sys.argv[1]
+
+cmd = "target/release/negotiation-bench -n1000 -m {} -o {}"
+
+commit = sh.run("git rev-parse --short HEAD", shell=True, capture_output=True)
+commit = commit.stdout.decode('utf8').strip()
+outfile = f"{outdir}/{commit}-negbench.data"
+print(outfile)
+
+for mode in ['0r', '1r', 'rd:"redis://127.0.0.1:6379"']:
+    print(mode, outfile)
+    sh.run(cmd.format(mode, outfile), env = { 'RUST_LOG': 'info' }, shell=True)
