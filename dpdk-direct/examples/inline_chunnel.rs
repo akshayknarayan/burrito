793ca4c2cbceaf000647be33c1a45c280c5add2e
Akshay Narayan <akshayn@mit.edu>
2022-07-16 01:42:21 +0000

    dpdk-inline: multiple client threads

--- dpdk-direct/examples/inline_chunnel.rs
+++ dpdk-direct/examples/inline_chunnel.rs
@@ -1,7 +1,7 @@
 use bertha::{ChunnelConnection, ChunnelConnector, ChunnelListener};
 use color_eyre::eyre::{ensure, Report};
-use dpdk_direct::{DpdkInlineChunnel, DpdkInlineReqChunnel};
-use futures_util::TryStreamExt;
+use dpdk_direct::{DpdkInlineChunnel, DpdkInlineCn, DpdkInlineReqChunnel};
+use futures_util::{future::Either, TryStreamExt};
 use quanta::Instant;
 use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4};
 use std::time::Duration;
@@ -33,74 +33,21 @@ async fn main() -> Result<(), Report> {
 
     if let Some(addr) = opt.ip_addr {
         let remote_addr = SocketAddr::V4(SocketAddrV4::new(addr, opt.port));
-        info!(?remote_addr, "Dpdk Inline Chunnel Test - Client");
+        let interarrival = std::time::Duration::from_micros(100);
+        info!(
+            ?remote_addr,
+            ?interarrival,
+            "Dpdk Inline Chunnel Test - Client"
+        );
 
         let mut ch = DpdkInlineChunnel::new(opt.datapath_cfg, 1)?;
-        let cn = ch.connect(()).await?;
-
-        let interarrival = std::time::Duration::from_micros(100);
-        let mut ticker = AsyncSpinTimer::new(interarrival);
-        let mut slots: Vec<_> = (0..16).map(|_| None).collect();
-        let mut msgs =
-            (0..opt.num_msgs).map(|i| (remote_addr, vec![(i % u8::MAX as usize) as u8; 32]));
-
-        use futures_util::future::Either;
-        let mut tot_msg_count = 0;
-        let mut tot_recv_count = 0;
-        let mut batch = Vec::with_capacity(16);
-
-        fn handle_received(
-            remote_addr: SocketAddr,
-            ms: &mut [Option<(SocketAddr, Vec<u8>)>],
-        ) -> Result<usize, Report> {
-            ms.iter_mut()
-                .map_while(Option::take)
-                .try_fold(0, |cnt, msg| {
-                    debug!(?msg, "received");
-                    ensure!(
-                        msg.0 == remote_addr,
-                        "received response from unexpected address"
-                    );
-                    Ok(cnt + 1)
-                })
-        }
-
-        loop {
-            let t = ticker.wait();
-            tokio::pin!(t);
-            match futures_util::future::select(t, cn.recv(&mut slots[..])).await {
-                Either::Left((mut num_msgs, _)) => {
-                    while num_msgs > 0 {
-                        if let Some(msg) = msgs.next() {
-                            batch.push(msg);
-                            num_msgs -= 1;
-                            tot_msg_count += 1;
-                        } else {
-                            break;
-                        }
-                    }
-
-                    debug!(?tot_msg_count, "sending");
-                    cn.send(batch.drain(..)).await?;
-                    if num_msgs > 0 {
-                        info!("done sending");
-                        break;
-                    }
-                }
-                Either::Right((ms, _)) => {
-                    tot_recv_count += handle_received(remote_addr, ms?)?;
-                    debug!(?tot_recv_count, "received");
-                }
-            }
-        }
-
-        while tot_recv_count < opt.num_msgs {
-            let ms = cn.recv(&mut slots[..]).await?;
-            tot_recv_count += handle_received(remote_addr, ms)?;
-            debug!(?tot_recv_count, "received messages");
-        }
-
-        info!("done");
+        let cn1 = ch.connect(()).await?;
+        let cn2 = ch.connect(()).await?;
+        let cl1 = client(cn1, opt.num_msgs / 2, interarrival, remote_addr);
+        let cl2 = client(cn2, opt.num_msgs / 2, interarrival, remote_addr);
+        let (r1, r2) = futures_util::join!(cl1, cl2);
+        r1?;
+        r2
     } else {
         let local_addr = SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::UNSPECIFIED, opt.port));
         info!(?local_addr, "Dpdk Inline Chunnel Test - Server");
@@ -125,8 +72,76 @@ async fn main() -> Result<(), Report> {
                     debug!(?recv_count, "received message burst");
                 }
             })
-            .await?;
+            .await
     }
+}
+
+async fn client(
+    cn: DpdkInlineCn,
+    num_msgs: usize,
+    interarrival: Duration,
+    remote_addr: SocketAddr,
+) -> Result<(), Report> {
+    let mut ticker = AsyncSpinTimer::new(interarrival);
+    let mut slots: Vec<_> = (0..16).map(|_| None).collect();
+    let mut msgs = (0..num_msgs).map(|i| (remote_addr, vec![(i % u8::MAX as usize) as u8; 32]));
+
+    let mut tot_msg_count = 0;
+    let mut tot_recv_count = 0;
+    let mut batch = Vec::with_capacity(16);
+
+    fn handle_received(
+        remote_addr: SocketAddr,
+        ms: &mut [Option<(SocketAddr, Vec<u8>)>],
+    ) -> Result<usize, Report> {
+        ms.iter_mut()
+            .map_while(Option::take)
+            .try_fold(0, |cnt, msg| {
+                debug!(?msg, "received");
+                ensure!(
+                    msg.0 == remote_addr,
+                    "received response from unexpected address"
+                );
+                Ok(cnt + 1)
+            })
+    }
+
+    loop {
+        let t = ticker.wait();
+        tokio::pin!(t);
+        match futures_util::future::select(t, cn.recv(&mut slots[..])).await {
+            Either::Left((mut num_msgs, _)) => {
+                while num_msgs > 0 {
+                    if let Some(msg) = msgs.next() {
+                        batch.push(msg);
+                        num_msgs -= 1;
+                        tot_msg_count += 1;
+                    } else {
+                        break;
+                    }
+                }
+
+                debug!(?tot_msg_count, "sending");
+                cn.send(batch.drain(..)).await?;
+                if num_msgs > 0 {
+                    info!("done sending");
+                    break;
+                }
+            }
+            Either::Right((ms, _)) => {
+                tot_recv_count += handle_received(remote_addr, ms?)?;
+                debug!(?tot_recv_count, "received");
+            }
+        }
+    }
+
+    while tot_recv_count < num_msgs {
+        let ms = cn.recv(&mut slots[..]).await?;
+        tot_recv_count += handle_received(remote_addr, ms)?;
+        debug!(?tot_recv_count, "received messages");
+    }
+
+    info!("done");
     Ok(())
 }
 
