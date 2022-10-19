b939b0d67c04c5da3da63ac10331b348be488537
Akshay Narayan <akshayn@icsi.berkeley.edu>
2022-10-19 00:33:48 +0000

    inline_chunnel: remove recv-timeout logic

--- dpdk-direct/examples/inline_chunnel.rs
+++ dpdk-direct/examples/inline_chunnel.rs
@@ -253,25 +253,6 @@ async fn client(
         debug!(?tot_recv_count, "received burst");
     }
 
-    //while tot_recv_count < num_msgs {
-    //    let sleep = tokio::time::sleep(std::time::Duration::from_secs(10));
-    //    tokio::pin!(sleep);
-    //    match futures_util::future::select(sleep, cn.recv(&mut slots[..])).await {
-    //        Either::Left((_, _)) => {
-    //            // timed out recv. exit
-    //            error!("timed out on wait. exiting");
-    //            break;
-    //        }
-    //        Either::Right((Ok(ms), _)) => {
-    //            tot_recv_count += handle_received(remote_addr, ms)?;
-    //            debug!(?tot_recv_count, "received burst");
-    //        }
-    //        Either::Right((Err(err), _)) => {
-    //            return Err(err);
-    //        }
-    //    }
-    //}
-
     info!("done");
     Ok(())
 }
