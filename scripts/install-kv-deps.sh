bbbd4b305fd05f54dfd8f3c5d735b385919c9e72
Akshay Narayan <akshayn@icsi.berkeley.edu>
2023-03-28 14:30:17 -0700

    scripts: only install docker if needed

--- scripts/install-kv-deps.sh
+++ scripts/install-kv-deps.sh
@@ -21,17 +21,21 @@ then
 fi
 
 # docker
-sudo apt install -y ca-certificates curl gnupg lsb-release 
+if ! command -v docker &> /dev/null
+then
+    sudo apt install -y ca-certificates curl gnupg lsb-release 
 
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor --yes -o /etc/apt/keyrings/docker.gpg
+    sudo mkdir -p /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor --yes -o /etc/apt/keyrings/docker.gpg
 
-echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    echo \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+        $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt update
-sudo apt install -y docker-ce docker-ce-cli containerd.io
+    sudo apt update
+    sudo apt install -y docker-ce docker-ce-cli containerd.io
+fi
+
 popd
 
 # ycsbc-mock
