b16749d63f0312468f21fbcb020fdf640b248ac1
Akshay Narayan <akshayn@icsi.berkeley.edu>
2023-07-31 17:04:30 -0700

    kafka: Update for new chunnel api

--- kafka/src/lib.rs
+++ kafka/src/lib.rs
@@ -2,6 +2,7 @@
 
 use bertha::ChunnelConnection;
 use color_eyre::eyre::{eyre, Report, WrapErr};
+use futures_util::StreamExt;
 use rdkafka::{
     admin::AdminClient,
     consumer::{stream_consumer::StreamConsumer, Consumer},
@@ -118,34 +119,49 @@ impl KafkaChunnel {
 impl ChunnelConnection for KafkaChunnel {
     type Data = (KafkaAddr, String);
 
-    fn send(
-        &self,
-        (KafkaAddr { topic_id, group }, body): Self::Data,
-    ) -> Pin<Box<dyn Future<Output = Result<(), Report>> + Send + 'static>> {
-        let producer = self.producer.clone();
+    fn send<'cn, B>(
+        &'cn self,
+        burst: B,
+        //: Self::Data,
+    ) -> Pin<Box<dyn Future<Output = Result<(), Report>> + Send + 'cn>>
+    where
+        B: IntoIterator<Item = Self::Data> + Send + 'cn,
+        <B as IntoIterator>::IntoIter: Send,
+    {
         Box::pin(async move {
-            let fr = FutureRecord {
-                topic: &topic_id,
-                partition: None,
-                payload: Some(&body),
-                key: group.as_ref(),
-                timestamp: Some(time()),
-                headers: None,
-            };
+            // producer.send() uses internal buffering, so it is not expected to be necessary
+            // to await the sends in this loop concurrently.
+            for (KafkaAddr { topic_id, group }, body) in burst {
+                let fr = FutureRecord {
+                    topic: &topic_id,
+                    partition: None,
+                    payload: Some(&body),
+                    key: group.as_ref(),
+                    timestamp: Some(time()),
+                    headers: None,
+                };
 
-            trace!(?topic_id, "sending message");
-            match producer.send(fr, rdkafka::util::Timeout::Never).await {
-                Ok((_, _)) => Ok(()),
-                Err((e, _)) => Err::<_, Report>(e.into()).wrap_err("kafka send"),
+                trace!(?topic_id, "sending message");
+                match self.producer.send(fr, rdkafka::util::Timeout::Never).await {
+                    Ok((_, _)) => (),
+                    Err((e, _)) => return Err::<_, Report>(e.into()).wrap_err("kafka send"),
+                }
             }
+
+            Ok(())
         })
     }
 
-    fn recv(&self) -> Pin<Box<dyn Future<Output = Result<Self::Data, Report>> + Send + 'static>> {
-        let consumer = Arc::clone(&self.consumer);
+    fn recv<'cn, 'buf>(
+        &'cn self,
+        msgs_buf: &'buf mut [Option<Self::Data>],
+    ) -> Pin<Box<dyn Future<Output = Result<&'buf mut [Option<Self::Data>], Report>> + Send + 'cn>>
+    where
+        'buf: 'cn,
+    {
         Box::pin(
             async move {
-                let sub = consumer.subscription()?;
+                let sub = self.consumer.subscription()?;
                 let topics: Vec<_> = sub
                     .elements()
                     .into_iter()
@@ -153,28 +169,43 @@ impl ChunnelConnection for KafkaChunnel {
                     .collect();
                 trace!(?topics, "receiving");
                 use rdkafka::message::Message;
+                let kafka_stream = self.consumer.stream();
                 trace!("waiting on kafka stream");
-                let msg = consumer.recv().await.wrap_err("kafka recv stream")?;
-                let key = if let Some(Ok(s)) = msg.key_view::<str>() {
-                    Some(s.to_owned())
-                } else {
-                    None
-                };
-                let body = if let Some(Ok(s)) = msg.payload_view::<str>() {
-                    s.to_owned()
-                } else {
-                    String::new()
-                };
 
-                let topic_id = msg.topic().to_owned();
-                trace!(?topic_id, "got msg");
-                Ok((
-                    KafkaAddr {
-                        topic_id,
-                        group: key,
-                    },
-                    body,
-                ))
+                let mut slot_idx = 0;
+                for (msg, slot) in kafka_stream
+                    .ready_chunks(msgs_buf.len())
+                    .next()
+                    .await
+                    .expect("MessageStream never returns None")
+                    .into_iter()
+                    .zip(msgs_buf.iter_mut())
+                {
+                    let msg = msg?;
+                    let key = if let Some(Ok(s)) = msg.key_view::<str>() {
+                        Some(s.to_owned())
+                    } else {
+                        None
+                    };
+                    let body = if let Some(Ok(s)) = msg.payload_view::<str>() {
+                        s.to_owned()
+                    } else {
+                        String::new()
+                    };
+
+                    let topic_id = msg.topic().to_owned();
+                    trace!(?topic_id, "got msg");
+                    *slot = Some((
+                        KafkaAddr {
+                            topic_id,
+                            group: key,
+                        },
+                        body,
+                    ));
+                    slot_idx += 1;
+                }
+
+                Ok(&mut msgs_buf[..slot_idx])
             }
             .instrument(debug_span!("kafka_recv")),
         )
@@ -192,7 +223,11 @@ fn gen_resource_id() -> String {
     use rand::Rng;
     let rng = rand::thread_rng();
     "k".chars()
-        .chain(rng.sample_iter(&rand::distributions::Alphanumeric).take(10))
+        .chain(
+            rng.sample_iter(&rand::distributions::Alphanumeric)
+                .map(Into::into)
+                .take(10),
+        )
         .collect()
 }
 
@@ -211,14 +246,20 @@ mod test {
     #[ignore]
     #[test]
     fn kafka_send_recv() {
-        // relies on Kafka running
+        // relies on Kafka running. To start:
+        // 1. wget https://downloads.apache.org/kafka/3.5.1/kafka_2.13-3.5.1.tgz
+        // 2. tar -xzf kafka_2.13-3.5.1.tgz
+        // 3. ./kafka_2.13-3.5.1/bin/zookeeper-server-start.sh ./kafka_2.13-3.5.1/config/zookeeper.properties
+        // 4. ./kafka_2.13-3.5.1/bin/kafka-server-start.sh ./kafka_2.13-3.5.1/config/server.properties
+
         let kafka_addr =
             std::env::var("KAFKA_SERVER").unwrap_or_else(|_| "localhost:9092".to_owned());
         let subscriber = tracing_subscriber::registry()
             .with(tracing_subscriber::fmt::layer())
-            .with(tracing_subscriber::EnvFilter::from_default_env())
+            .with(tracing_subscriber::filter::EnvFilter::from_default_env())
             .with(ErrorLayer::default());
-        let _guard = subscriber.set_default();
+        let d = tracing::Dispatch::new(subscriber);
+        d.init();
         color_eyre::install().unwrap_or(());
 
         let rt = tokio::runtime::Builder::new_current_thread()
@@ -235,17 +276,19 @@ mod test {
 
                 let ch = KafkaChunnel::new(&kafka_addr)?;
                 ch.listen(&[&topic_name])?;
-                ch.send((
+                ch.send(std::iter::once((
                     KafkaAddr {
                         topic_id: topic_name.clone(),
                         group: None,
                     },
                     "test message".to_string(),
-                ))
+                )))
                 .await
                 .wrap_err("kafka queue send")?;
                 info!("sent");
-                let (_, msg) = ch.recv().await.wrap_err("kafka queue recv")?;
+                let mut slot = [None];
+                let msgs = ch.recv(&mut slot[..]).await.wrap_err("kafka queue recv")?;
+                let (_, msg) = msgs[0].take().unwrap();
                 info!("received");
                 assert_eq!(&msg, "test message");
 
