7cdac25d38f35abbbeb5b7379a253beecb253af3
Akshay Narayan <akshayn@icsi.berkeley.edu>
2023-09-06 18:39:50 -0700

    tcp: Wrapping version of TcpChunnel for stacks

--- tcp/src/lib.rs
+++ tcp/src/lib.rs
@@ -5,17 +5,24 @@
 
 #![feature(iter_array_chunks)]
 
-use bertha::{ChunnelConnection, ChunnelConnector, ChunnelListener};
+use bertha::{Chunnel, ChunnelConnection, ChunnelConnector, ChunnelListener, Negotiate};
 use color_eyre::eyre::{bail, ensure, eyre, Report, WrapErr};
 use futures_util::{
     future::{ready, Ready},
     stream::{Stream, StreamExt, TryStreamExt},
 };
-use std::cmp::Ordering;
-use std::net::SocketAddr;
+use std::{cmp::Ordering, collections::HashMap};
 use std::{future::Future, pin::Pin};
-use tokio::net::{TcpListener, TcpStream};
+use std::{
+    net::SocketAddr,
+    sync::{Arc, Mutex},
+};
+use tokio::{
+    net::{TcpListener, TcpSocket, TcpStream},
+    sync::Mutex as TokioMutex,
+};
 use tokio_stream::wrappers::TcpListenerStream;
+use tracing::debug;
 
 /// TCP Chunnel connector.
 ///
@@ -60,7 +67,7 @@ impl ChunnelListener for TcpChunnel<false> {
 
 impl ChunnelListener for TcpChunnel<true> {
     type Addr = SocketAddr;
-    type Connection = IgnoreAddr<SocketAddr, TcpCn>;
+    type Connection = IgnoreAddr<TcpCn>;
     type Future = Ready<Result<Self::Stream, Self::Error>>;
     type Stream =
         Pin<Box<dyn Stream<Item = Result<Self::Connection, Self::Error>> + Send + 'static>>;
@@ -95,7 +102,7 @@ impl ChunnelConnector for TcpChunnel<false> {
 
 impl ChunnelConnector for TcpChunnel<true> {
     type Addr = SocketAddr;
-    type Connection = IgnoreAddr<SocketAddr, TcpCn>;
+    type Connection = IgnoreAddr<TcpCn>;
     type Future =
         Pin<Box<dyn Future<Output = Result<Self::Connection, Self::Error>> + Send + 'static>>;
     type Error = Report;
@@ -103,21 +110,136 @@ impl ChunnelConnector for TcpChunnel<true> {
     fn connect(&mut self, a: Self::Addr) -> Self::Future {
         Box::pin(async move {
             let sk = TcpStream::connect(a).await?;
+            debug!(peer = ?sk.peer_addr(), local = ?sk.local_addr(), "established connection");
             Ok(IgnoreAddr(a, TcpCn::new(sk)))
         })
     }
 }
 
-#[derive(Debug, Clone)]
-pub struct IgnoreAddr<A, C>(pub A, pub C);
+#[derive(Clone, Debug)]
+pub struct TcpChunnelWrapServer {
+    listen_addr: SocketAddr,
+    cached_conns: Arc<Mutex<HashMap<SocketAddr, TcpStream>>>,
+    listen_stream: Arc<TokioMutex<TcpListenerStream>>,
+}
 
-impl<A, C, D> ChunnelConnection for IgnoreAddr<A, C>
+impl Negotiate for TcpChunnelWrapServer {
+    type Capability = ();
+
+    fn guid() -> u64 {
+        0x614489199455a800
+    }
+}
+
+impl TcpChunnelWrapServer {
+    pub fn new(listen_addr: SocketAddr) -> Result<Self, Report> {
+        let listen_stream = TcpListenerStream::new(make_tcp_listener(listen_addr)?);
+        Ok(Self {
+            listen_addr,
+            cached_conns: Default::default(),
+            listen_stream: Arc::new(TokioMutex::new(listen_stream)),
+        })
+    }
+}
+
+impl<C> Chunnel<C> for TcpChunnelWrapServer
 where
-    C: ChunnelConnection<Data = D> + Send + Sync + 'static,
-    A: Clone + PartialEq + std::fmt::Debug + Send + Sync + 'static,
-    D: Send,
+    C: Connected,
 {
-    type Data = (A, D);
+    type Error = Report;
+    type Connection = IgnoreAddr<TcpCn>;
+    type Future =
+        Pin<Box<dyn Future<Output = Result<Self::Connection, Self::Error>> + Send + 'static>>;
+    fn connect_wrap(&mut self, inner: C) -> Self::Future {
+        if inner.local_addr() != self.listen_addr {
+            return Box::pin(ready(Err(eyre!(
+                "TcpChunnelWrapServer: local address mismatched"
+            ))));
+        }
+
+        let peer_addr = inner.peer_addr();
+        if peer_addr.is_none() {
+            return Box::pin(ready(Err(eyre!(
+                "TcpChunnelWrapServer: no peer address for inner connection"
+            ))));
+        }
+        let peer_addr = peer_addr.unwrap();
+        {
+            let mut cached_conns_g = self.cached_conns.lock().unwrap();
+            if let Some(cn) = cached_conns_g.remove(&peer_addr) {
+                return Box::pin(ready(Ok(IgnoreAddr(peer_addr, TcpCn::new(cn)))));
+            }
+        }
+
+        let cached_conns = self.cached_conns.clone();
+        let listen_stream = self.listen_stream.clone();
+        let listen_addr = self.listen_addr;
+        Box::pin(async move {
+            let mut listen_stream_g = listen_stream.lock_owned().await;
+            loop {
+                debug!(?peer_addr, ?listen_addr, "waiting for tcp connection");
+                let cn = listen_stream_g.try_next().await?.unwrap();
+                let peer = cn.peer_addr().unwrap();
+                if peer == peer_addr {
+                    return Ok(IgnoreAddr(peer_addr, TcpCn::new(cn)));
+                } else {
+                    let mut cached_conns_g = cached_conns.lock().unwrap();
+                    cached_conns_g.insert(peer, cn);
+                }
+            }
+        })
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+pub struct TcpChunnelWrapClient {
+    addr: SocketAddr,
+}
+
+impl TcpChunnelWrapClient {
+    pub fn new(addr: SocketAddr) -> Self {
+        Self { addr }
+    }
+}
+
+impl Negotiate for TcpChunnelWrapClient {
+    type Capability = ();
+
+    fn guid() -> u64 {
+        0x614489199455a800
+    }
+}
+
+impl<C> Chunnel<C> for TcpChunnelWrapClient
+where
+    C: Connected,
+{
+    type Error = Report;
+    type Connection = IgnoreAddr<TcpCn>;
+    type Future =
+        Pin<Box<dyn Future<Output = Result<Self::Connection, Self::Error>> + Send + 'static>>;
+    fn connect_wrap(&mut self, inner: C) -> Self::Future {
+        let inner_local_addr = inner.local_addr();
+        let addr = self.addr;
+        debug!(?self.addr, "connecting tcp");
+        Box::pin(async move {
+            let sk = TcpSocket::new_v4()?;
+            sk.bind(inner_local_addr)?;
+            let sk = sk.connect(addr).await?;
+            debug!(peer = ?sk.peer_addr(), local = ?sk.local_addr(), "established connection");
+            Ok(IgnoreAddr(addr, TcpCn::new(sk)))
+        })
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct IgnoreAddr<C>(pub SocketAddr, pub C);
+
+impl<C> ChunnelConnection for IgnoreAddr<C>
+where
+    C: ChunnelConnection<Data = Vec<u8>> + Send + Sync + 'static,
+{
+    type Data = (SocketAddr, Vec<u8>);
 
     fn send<'cn, B>(
         &'cn self,
@@ -138,7 +260,8 @@ where
         'buf: 'cn,
     {
         Box::pin(async move {
-            let mut with_addr_slots: Vec<_> = (0..msgs_buf.len()).map(|_| None).collect();
+            let mut with_addr_slots: Vec<Option<Vec<u8>>> =
+                msgs_buf.iter_mut().map(|v| v.take().map(|x| x.1)).collect();
             let ms = self.1.recv(&mut with_addr_slots[..]).await?;
             let mut ret_len = 0;
             for (msg, slot) in ms
@@ -472,10 +595,14 @@ where
 
 #[cfg(test)]
 mod t {
+    use crate::{TcpChunnelWrapClient, TcpChunnelWrapServer};
+
     use super::TcpChunnel;
+    use bertha::udp::{UdpReqChunnel, UdpSkChunnel};
     use bertha::{ChunnelConnection, ChunnelConnector, ChunnelListener};
     use color_eyre::eyre::{bail, ensure, Report, WrapErr};
     use futures_util::TryStreamExt;
+    use std::net::{Ipv4Addr, SocketAddr};
     use std::sync::Arc;
     use std::sync::Once;
     use tokio::sync::Barrier;
@@ -486,7 +613,7 @@ mod t {
     pub static COLOR_EYRE: Once = Once::new();
 
     #[test]
-    fn tcp_chunnel() {
+    fn basic() {
         let subscriber = tracing_subscriber::registry()
             .with(tracing_subscriber::fmt::layer())
             .with(tracing_subscriber::EnvFilter::from_default_env())
@@ -558,6 +685,86 @@ mod t {
             Ok(())
         })
         .unwrap();
+    }
+
+    #[test]
+    fn wrap() {
+        let subscriber = tracing_subscriber::registry()
+            .with(tracing_subscriber::fmt::layer())
+            .with(tracing_subscriber::EnvFilter::from_default_env())
+            .with(ErrorLayer::default());
+        let _guard = subscriber.set_default();
+        COLOR_EYRE.call_once(|| color_eyre::install().unwrap_or(()));
+        let rt = tokio::runtime::Builder::new_current_thread()
+            .enable_all()
+            .build()
+            .unwrap();
+        let addr = "127.0.0.1:58281".parse().unwrap();
+
+        #[tracing::instrument(level = "info", err)]
+        async fn server(addr: SocketAddr) -> Result<(), Report> {
+            let stack = TcpChunnelWrapServer::new(addr)?;
+            let mut base = UdpReqChunnel;
+            let base_st = base.listen(addr).await?;
+            let st = bertha::negotiate_server(stack, base_st).await?;
+            st.try_for_each_concurrent(None, |cn| async move {
+                info!("starting connection");
+                let mut msgs: Vec<_> = (0..8)
+                    .map(|_| {
+                        Some((
+                            SocketAddr::new(Ipv4Addr::UNSPECIFIED.into(), 0),
+                            Vec::with_capacity(2048),
+                        ))
+                    })
+                    .collect();
+                loop {
+                    let ms = cn.recv(&mut msgs[..]).await?;
+                    debug!(?ms, "received from client");
+                    cn.send(ms.iter_mut().map_while(Option::take)).await?;
+                }
+            })
+            .await?;
+            unreachable!()
+        }
+
+        rt.block_on(async move {
+            let jh = tokio::spawn(server(addr));
+
+            let stack = TcpChunnelWrapClient::new(addr);
+            let mut base = UdpSkChunnel;
+            let base_cn = base.connect(addr).await?;
+            let cn = bertha::negotiate_client(stack, base_cn, addr).await?;
+
+            let mut msgs: Vec<_> = (0..8)
+                .map(|_| {
+                    Some((
+                        SocketAddr::new(Ipv4Addr::UNSPECIFIED.into(), 0),
+                        Vec::with_capacity(2048),
+                    ))
+                })
+                .collect();
+            for i in 0..10 {
+                debug!(?i, "send");
+                cn.send((0..4).map(|i| (addr, u64::to_le_bytes(i).to_vec())))
+                    .await?;
+
+                let ms = cn.recv(&mut msgs[..]).await?;
+                debug!(?i, ?ms, "got echo");
+                for (i, m) in ms.iter_mut().map_while(Option::take).enumerate() {
+                    ensure!(
+                        i == u64::from_le_bytes(m.1[0..8].try_into().unwrap()) as usize,
+                        "wrong message"
+                    );
+                }
+            }
+
+            if jh.is_finished() {
+                return jh.await.unwrap();
+            }
+
+            Ok(())
+        })
+        .unwrap()
     }
 
     #[test]
